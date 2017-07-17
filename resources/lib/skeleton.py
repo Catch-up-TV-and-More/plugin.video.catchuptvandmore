a0e71585f39c4ee40617e535e2e846ce391ca965
wwark <wark@free.fr>
2017-07-17 21:19:14 +0200

    Fix Name

--- resources/lib/skeleton.py
+++ resources/lib/skeleton.py
@@ -52,7 +52,7 @@ channels = {
         'channels.fr.numero23.numero23': 'Numéro 23',
         'channels.fr.nrj.cherie25': 'Chérie 25',
 	'channels.fr.pluzz.la_1ere': 'La 1ère (Outre-Mer)',
-	'channels.fr.pluzz.franceinfo': 'France Info)'
+	'channels.fr.pluzz.franceinfo': 'France Info'
 
     },
 
