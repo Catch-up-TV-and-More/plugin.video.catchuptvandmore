8e0cd1539ec9a5b733b68973bf392bad80c77f5e
wwark <wark@free.fr>
2017-09-03 10:16:30 +0200

    GROUPE TF1 | Add replay LCI

--- resources/lib/channels/fr/tf1.py
+++ resources/lib/channels/fr/tf1.py
@@ -25,9 +25,11 @@ from bs4 import BeautifulSoup as bs
 from resources.lib import utils
 from resources.lib import common
 import json
+import re
 
 # TODO
 # Rework QUALITY
+# Replay LCI (Add date, aired, year, fix some elements)
 
 # Initialize GNU gettext emulation in addon
 # This allows to use UI strings from addon’s English
@@ -40,6 +42,9 @@ url_token = 'http://api.wat.tv/services/Delivery'
 url_live_tv = 'https://www.tf1.fr/%s/direct'
 url_live_info = 'https://api.mytf1.tf1.fr/live/2/%s'
 url_api_image = 'http://api.mytf1.tf1.fr/image'
+
+url_lci_replay = "http://www.lci.fr/emissions"
+url_lci_root = "http://www.lci.fr"
 
 secret_key = 'W3m0#1mFI'
 app_name = 'sdk/Iphone/1.0'
@@ -66,21 +71,20 @@ def channel_entry(params):
     else:
         return None
 
-@common.plugin.cached(common.cache_time)
+#@common.plugin.cached(common.cache_time)
 def mode_replay_live(params):
     modes = []
     
     # Add Replay 
-    if params.channel_name != 'lci':
-	modes.append({
-	    'label' : 'Replay',
-	    'url': common.plugin.get_url(
-		action='channel_entry',
-		next='list_shows_1',
-		category='%s Replay' % params.channel_name.upper(),
-		window_title='%s Replay' % params.channel_name.upper()
-	    ),
-	})
+    modes.append({
+	'label' : 'Replay',
+	'url': common.plugin.get_url(
+	    action='channel_entry',
+	    next='list_shows_1',
+	    category='%s Replay' % params.channel_name.upper(),
+	    window_title='%s Replay' % params.channel_name.upper()
+	),
+    })
     
     # Add Live 
     if params.channel_name != 'tfou' and params.channel_name != 'xtra':
@@ -102,70 +106,106 @@ def mode_replay_live(params):
         ),
     )
 
-@common.plugin.cached(common.cache_time)
+#@common.plugin.cached(common.cache_time)
 def list_shows(params):
     shows = []
 
-    url = ''.join((
-        url_root,
-        params.channel_name,
-        '/programmes-tv'))
-    file_path = utils.download_catalog(
-        url,
-        params.channel_name + '.html')
-    root_html = open(file_path).read()
-    root_soup = bs(root_html, 'html.parser')
+    if params.channel_name == 'lci':
+	
+	file_path = utils.download_catalog(
+	    url_lci_replay,
+	    params.channel_name + '.html')
+	root_html = open(file_path).read()
+	root_soup = bs(root_html, 'html.parser')
+	
+	if params.next == 'list_shows_1':
+	    programs_soup = root_soup.find(
+		'ul',
+		attrs={'class': 'topic-chronology-milestone-component'})
+	    for program in programs_soup.find_all('li'):
+		program_url = url_lci_root + program.find('a')['href'].encode('utf-8')
+		program_name = program.find(
+		    'h2',
+		    class_='text-block').get_text().encode('utf-8')
+		img = program.find_all('source')[0]
+		try:
+		    img = img['data-srcset'].encode('utf-8')
+		except:
+		    img = img['srcset'].encode('utf-8')
 
-    if params.next == 'list_shows_1':
-        categories_soup = root_soup.find(
-            'ul',
-            attrs={'class': 'filters_2 contentopen'})
-        for category in categories_soup.find_all('a'):
-            category_name = category.get_text().encode('utf-8')
-            category_url = category['data-target'].encode('utf-8')
+		img = img.split(',')[0].split(' ')[0]
 
-            shows.append({
-                'label': category_name,
-                'url': common.plugin.get_url(
-                    action='channel_entry',
-                    category=category_url,
-                    next='list_shows_2',
-                    window_title=category_name
-                )
-            })
+		shows.append({
+		    'label': program_name,
+		    'thumb': img,
+		    'url': common.plugin.get_url(
+			action='channel_entry',
+			program_url=program_url,
+			next='list_videos',
+			window_title=program_name
+		    )
+		})
+    else:
+	url = ''.join((
+	    url_root,
+	    params.channel_name,
+	    '/programmes-tv'))
+	file_path = utils.download_catalog(
+	    url,
+	    params.channel_name + '.html')
+	root_html = open(file_path).read()
+	root_soup = bs(root_html, 'html.parser')
 
-    elif params.next == 'list_shows_2':
-        programs_soup = root_soup.find(
-            'ul',
-            attrs={'id': 'js_filter_el_container'})
-        for program in programs_soup.find_all('li'):
-            category = program['data-type'].encode('utf-8')
-            if params.category == category or params.category == 'all':
-                program_url = program.find(
-                    'div',
-                    class_='description')
-                program_url = program_url.find('a')['href'].encode('utf-8')
-                program_name = program.find(
-                    'p',
-                    class_='program').get_text().encode('utf-8')
-                img = program.find('img')
-                try:
-                    img = img['data-srcset'].encode('utf-8')
-                except:
-                    img = img['srcset'].encode('utf-8')
+	if params.next == 'list_shows_1':
+	    categories_soup = root_soup.find(
+		'ul',
+		attrs={'class': 'filters_2 contentopen'})
+	    for category in categories_soup.find_all('a'):
+		category_name = category.get_text().encode('utf-8')
+		category_url = category['data-target'].encode('utf-8')
 
-                img = 'http:' + img.split(',')[-1].split(' ')[0]
+		shows.append({
+		    'label': category_name,
+		    'url': common.plugin.get_url(
+			action='channel_entry',
+			category=category_url,
+			next='list_shows_2',
+			window_title=category_name
+		    )
+		})
 
-                shows.append({
-                    'label': program_name,
-                    'thumb': img,
-                    'url': common.plugin.get_url(
-                        action='channel_entry',
-                        program_url=program_url,
-                        next='list_videos_categories',
-                        window_title=program_name
-                    )
-                })
+	elif params.next == 'list_shows_2':
+	    programs_soup = root_soup.find(
+		'ul',
+		attrs={'id': 'js_filter_el_container'})
+	    for program in programs_soup.find_all('li'):
+		category = program['data-type'].encode('utf-8')
+		if params.category == category or params.category == 'all':
+		    program_url = program.find(
+			'div',
+			class_='description')
+		    program_url = program_url.find('a')['href'].encode('utf-8')
+		    program_name = program.find(
+			'p',
+			class_='program').get_text().encode('utf-8')
+		    img = program.find('img')
+		    try:
+			img = img['data-srcset'].encode('utf-8')
+		    except:
+			img = img['srcset'].encode('utf-8')
+
+		    img = 'http:' + img.split(',')[-1].split(' ')[0]
+
+		    shows.append({
+			'label': program_name,
+			'thumb': img,
+			'url': common.plugin.get_url(
+			    action='channel_entry',
+			    program_url=program_url,
+			    next='list_videos_categories',
+			    window_title=program_name
+			)
+		    })
 
     return common.plugin.create_listing(
         shows,
@@ -176,7 +216,7 @@ def list_shows(params):
     )
 
 
-@common.plugin.cached(common.cache_time)
+#@common.plugin.cached(common.cache_time)
 def list_videos_categories(params):
     videos_categories = []
     url = ''.join((
@@ -214,109 +254,167 @@ def list_videos_categories(params):
 def list_videos(params):
     videos = []
 
-    url = ''.join((
-        params.program_url,
-        '/videos/',
-        '?filter=',
-        params.category_id))
-    program_html = utils.get_webcontent(url)
-    program_soup = bs(program_html, 'html.parser')
+    if params.channel_name == 'lci':
+	program_html = utils.get_webcontent(params.program_url)
+	program_soup = bs(program_html, 'html.parser')
+	
+	list_replay = program_soup.find_all(
+	    'a',
+	    class_='topic-emission-extract-block-image trackXiti')
 
-    grid = program_soup.find(
-        'ul',
-        class_='grid')
+	for replay in list_replay:
+	
+	    title = replay.find_all('img')[0].get('alt').encode('utf-8')
+	    duration = 0
+	    img = replay.find_all('source')[0]
+	    try:
+		img = img['data-srcset'].encode('utf-8')
+	    except:
+		img = img['srcset'].encode('utf-8')
 
-    for li in grid.find_all('li'):
-        video_type_string = li.find('div', class_='description').find('a')['data-xiti-libelle'].encode('utf-8')
-        video_type_string = video_type_string.split('-')[0]
+	    img = img.split(',')[0].split(' ')[0]
+	    program_id = url_lci_root + replay.get('href').encode('utf-8')
 
-        if 'Playlist' not in video_type_string:
-            title = li.find(
-                'p',
-                class_='title').get_text().encode('utf-8')
-
-            try:
-                stitle = li.find(
-                    'p',
-                    class_='stitle').get_text().encode('utf-8')
-            except:
-                stitle = ''
-
-            try:
-                duration_soup = li.find(
-                    'p',
-                    class_='uptitle').find(
-                        'span',
-                        class_='momentDate')
-                duration = int(duration_soup.get_text().encode('utf-8'))
-            except:
-                duration = 0
-
-            img = li.find('img')
-            try:
-                img = img['data-srcset'].encode('utf-8')
-            except:
-                img = img['srcset'].encode('utf-8')
-
-            img = 'http:' + img.split(',')[-1].split(' ')[0]
-
-            try:
-                date_soup = li.find(
-                    'div',
-                    class_='text').find(
-                        'p',
-                        class_='uptitle').find('span')
-
-                aired = date_soup['data-date'].encode('utf-8').split('T')[0]
-                day = aired.split('-')[2]
-                mounth = aired.split('-')[1]
-                year = aired.split('-')[0]
-                date = '.'.join((day, mounth, year))
-                # date : string (%d.%m.%Y / 01.01.2009)
-                # aired : string (2008-12-07)
-
-            except:
-                date = ''
-                aired = ''
-                year = 0
-
-            program_id = li.find('a')['href'].encode('utf-8')
-
-            info = {
-                'video': {
-                    'title': title,
-                    'plot': stitle,
-                    'aired': aired,
-                    'date': date,
-                    'duration': duration,
-                    'year': int(aired[:4]),
-                    'mediatype': 'tvshow'
-                }
-            }
+	    info = {
+		'video': {
+		    'title': title,
+		    #'plot': stitle,
+		    #'aired': aired,
+		    #'date': date,
+		    'duration': duration,
+		    #'year': int(aired[:4]),
+		    'mediatype': 'tvshow'
+		}
+	    }
 
 	    # Nouveau pour ajouter le menu pour télécharger la vidéo
 	    context_menu = []
 	    download_video = (
 		_('Download'),
-                'XBMC.RunPlugin(' + common.plugin.get_url(
-                    action='download_video',
-                    program_id=program_id) + ')'
-            )
-            context_menu.append(download_video)
-            # Fin
+		'XBMC.RunPlugin(' + common.plugin.get_url(
+		    action='download_video',
+		    program_id=program_id) + ')'
+	    )
+	    context_menu.append(download_video)
+	    # Fin
 
-            videos.append({
-                'label': title,
-                'thumb': img,
-                'url': common.plugin.get_url(
-                    action='channel_entry',
-                    next='play_r',
-                    program_id=program_id,
-                ),
-                'is_playable': True,
-                'info': info,
+	    videos.append({
+		'label': title,
+		'thumb': img,
+		'url': common.plugin.get_url(
+		    action='channel_entry',
+		    next='play_r',
+		    program_id=program_id,
+		),
+		'is_playable': True,
+		'info': info,
 		'context_menu': context_menu  #  A ne pas oublier pour ajouter le bouton "Download" à chaque vidéo
-            })
+	    })
+	
+    else:
+	url = ''.join((
+	    params.program_url,
+	    '/videos/',
+	    '?filter=',
+	    params.category_id))
+	program_html = utils.get_webcontent(url)
+	program_soup = bs(program_html, 'html.parser')
+
+	grid = program_soup.find(
+	    'ul',
+	    class_='grid')
+
+	for li in grid.find_all('li'):
+	    video_type_string = li.find('div', class_='description').find('a')['data-xiti-libelle'].encode('utf-8')
+	    video_type_string = video_type_string.split('-')[0]
+
+	    if 'Playlist' not in video_type_string:
+		title = li.find(
+		    'p',
+		    class_='title').get_text().encode('utf-8')
+
+		try:
+		    stitle = li.find(
+			'p',
+			class_='stitle').get_text().encode('utf-8')
+		except:
+		    stitle = ''
+
+		try:
+		    duration_soup = li.find(
+			'p',
+			class_='uptitle').find(
+			    'span',
+			    class_='momentDate')
+		    duration = int(duration_soup.get_text().encode('utf-8'))
+		except:
+		    duration = 0
+
+		img = li.find('img')
+		try:
+		    img = img['data-srcset'].encode('utf-8')
+		except:
+		    img = img['srcset'].encode('utf-8')
+
+		img = 'http:' + img.split(',')[-1].split(' ')[0]
+
+		try:
+		    date_soup = li.find(
+			'div',
+			class_='text').find(
+			    'p',
+			    class_='uptitle').find('span')
+
+		    aired = date_soup['data-date'].encode('utf-8').split('T')[0]
+		    day = aired.split('-')[2]
+		    mounth = aired.split('-')[1]
+		    year = aired.split('-')[0]
+		    date = '.'.join((day, mounth, year))
+		    # date : string (%d.%m.%Y / 01.01.2009)
+		    # aired : string (2008-12-07)
+
+		except:
+		    date = ''
+		    aired = ''
+		    year = 0
+
+		program_id = li.find('a')['href'].encode('utf-8')
+
+		info = {
+		    'video': {
+			'title': title,
+			'plot': stitle,
+			'aired': aired,
+			'date': date,
+			'duration': duration,
+			'year': int(aired[:4]),
+			'mediatype': 'tvshow'
+		    }
+		}
+
+		# Nouveau pour ajouter le menu pour télécharger la vidéo
+		context_menu = []
+		download_video = (
+		    _('Download'),
+		    'XBMC.RunPlugin(' + common.plugin.get_url(
+			action='download_video',
+			program_id=program_id) + ')'
+		)
+		context_menu.append(download_video)
+		# Fin
+
+		videos.append({
+		    'label': title,
+		    'thumb': img,
+		    'url': common.plugin.get_url(
+			action='channel_entry',
+			next='play_r',
+			program_id=program_id,
+		    ),
+		    'is_playable': True,
+		    'info': info,
+		    'context_menu': context_menu  #  A ne pas oublier pour ajouter le bouton "Download" à chaque vidéo
+		})
 
     return common.plugin.create_listing(
         videos,
@@ -404,9 +502,11 @@ def get_video_url(params):
 	    'div',
 	    class_='iframe_player')
 
-	data_src = iframe_player_soup['data-src'].encode('utf-8')
-
-	video_id = data_src[-8:]
+	if params.channel_name == 'lci':
+	    video_id = iframe_player_soup['data-watid'].encode('utf-8')
+	else:
+	    data_src = iframe_player_soup['data-src'].encode('utf-8')
+	    video_id = data_src[-8:]
 
 	timeserver = str(utils.get_webcontent(url_time))
 
