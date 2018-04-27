e139a5d5915c9fa966c5fd929bffe36dfeee246e
Marc Roy <wark@free.fr>
2018-04-28 00:23:22 +0900

    Replay CNews - First fix (website changed) - More work to do

--- resources/lib/root/channels/fr/cnews.py
+++ resources/lib/root/channels/fr/cnews.py
@@ -26,10 +26,13 @@ import json
 import ast
 from bs4 import BeautifulSoup as bs
 from resources.lib import utils
+from resources.lib import resolver
 from resources.lib import common
 
 
 # TO DO
+# Mettre le bouton More
+# Fix image of vidéos
 
 # URL :
 URL_ROOT_SITE = 'http://www.cnews.fr'
@@ -38,8 +41,9 @@ URL_ROOT_SITE = 'http://www.cnews.fr'
 URL_LIVE_CNEWS = URL_ROOT_SITE + '/direct'
 
 # Replay CNews
-URL_VIDEOS_CNEWS = URL_ROOT_SITE + '/videos/'
-URL_EMISSIONS_CNEWS = URL_ROOT_SITE + '/emissions'
+URL_REPLAY_CNEWS = URL_ROOT_SITE + '/replay'
+URL_EMISSIONS_CNEWS = URL_REPLAY_CNEWS + '/emissions'
+URL_VIDEOS_CNEWS = URL_REPLAY_CNEWS + '/videos'
 
 # Replay/Live => VideoId
 URL_INFO_CONTENT = 'https://secure-service.canal-plus.com/' \
@@ -67,21 +71,18 @@ def list_shows(params):
 
     if params.next == 'list_shows_1':
 
-        file_path = utils.download_catalog(
-            URL_VIDEOS_CNEWS,
-            '%s_categories.html' % (
-                params.channel_name))
-        root_html = open(file_path).read()
+        root_html = utils.get_webcontent(URL_REPLAY_CNEWS)
         root_soup = bs(root_html, 'html.parser')
-
-        menu_soup = root_soup.find('div', class_="nav-tabs-inner")
-
-        categories_soup = menu_soup.find_all('a')
+        menu_soup = root_soup.find('menu', class_="index-emission-menu")
+        categories_soup = menu_soup.find_all('li')
 
         for category in categories_soup:
 
             category_name = category.get_text().encode('utf-8')
-            category_url = URL_ROOT_SITE + category.get('href')
+            if 'mission' in category_name:
+                category_url = URL_EMISSIONS_CNEWS
+            else:
+                category_url = URL_VIDEOS_CNEWS
 
             if category_name != 'Les tops':
                 shows.append({
@@ -92,72 +93,7 @@ def list_shows(params):
                         action='replay_entry',
                         category_url=category_url,
                         category_name=category_name,
-                        next='list_shows_2',
-                        window_title=category_name
-                    )
-                })
-
-    elif params.next == 'list_shows_2':
-
-        if params.category_name == 'Les sujets':
-
-            file_path = utils.download_catalog(
-                params.category_url,
-                '%s_%s.html' % (
-                    params.channel_name, params.category_name))
-            root_html = open(file_path).read()
-            root_soup = bs(root_html, 'html.parser')
-            categories_soup = root_soup.find_all('a', class_="checkbox")
-
-            for category in categories_soup:
-
-                category_name = category.get_text().encode('utf-8')
-                category_url = URL_ROOT_SITE + category.get('href')
-
-                shows.append({
-                    'label': category_name,
-                    'url': common.PLUGIN.get_url(
-                        module_path=params.module_path,
-                        module_name=params.module_name,
-                        action='replay_entry',
-                        category_url=category_url,
-                        page="1",
-                        category_name=category_name,
-                        next='list_videos',
-                        window_title=category_name
-                    )
-                })
-        else:
-            # Find all emissions
-            file_path = utils.download_catalog(
-                URL_EMISSIONS_CNEWS,
-                '%s_ALL_EMISSION.html' % (
-                    params.channel_name))
-            root_html = open(file_path).read()
-            root_soup = bs(root_html, 'html.parser')
-
-            categories_soup = root_soup.find_all('article', class_="item")
-
-            for category in categories_soup:
-
-                category_name = category.find('h3').get_text().encode('utf-8')
-                category_url = URL_VIDEOS_CNEWS + \
-                    '/emissions' + \
-                    category.find('a').get('href').split('.fr')[1]
-                category_img = category.find('img').get('src').encode('utf-8')
-
-                shows.append({
-                    'label': category_name,
-                    'thumb': category_img,
-                    'fanart': category_img,
-                    'url': common.PLUGIN.get_url(
-                        module_path=params.module_path,
-                        module_name=params.module_name,
-                        action='replay_entry',
-                        category_url=category_url,
-                        page="1",
-                        category_name=category_name,
-                        next='list_videos',
+                        next='list_videos_1',
                         window_title=category_name
                     )
                 })
@@ -176,53 +112,29 @@ def list_shows(params):
 def list_videos(params):
     """Build videos listing"""
     videos = []
-    if 'previous_listing' in params:
-        videos = ast.literal_eval(params['previous_listing'])
 
     if params.channel_name == 'cnews':
 
-        url_page = params.category_url + '/page/%s' % params.page
-
-        file_path = utils.download_catalog(
-            url_page,
-            '%s_%s_%s.html' % (
-                params.channel_name, params.category_name, params.page))
-        root_html = open(file_path).read()
+        root_html = utils.get_webcontent(params.category_url)
         root_soup = bs(root_html, 'html.parser')
 
-        programs = root_soup.find_all('article', class_='item')
+        programs = root_soup.find_all('a', class_='video-item-wrapper')
+        programs += root_soup.find_all('a', class_='emission-item-wrapper')
 
         for program in programs:
-            title = program.find('h3').get_text().encode('utf-8')
-            thumb = program.find('img').get('src').encode('utf-8')
-            # Get Video_ID
-            video_html = utils.get_webcontent(
-                program.find('a').get('href').encode('utf-8'))
-            id = re.compile(r'videoId=(.*?)"').findall(video_html)[0]
-            # Get Description
-            datas_video = bs(video_html, 'html.parser')
-            description = datas_video.find(
-                'article', class_='entry-body').get_text().encode('utf-8')
+            title = program.find('img').get('alt').encode('utf-8')
+            thumb = program.find('img').get('src')
+            video_url = URL_ROOT_SITE + program.get('href')
             duration = 0
-
-            date = re.compile(
-                r'property="video:release_date" content="(.*?)"'
-            ).findall(video_html)[0].split('T')[0].split('-')
-            day = date[2]
-            mounth = date[1]
-            year = date[0]
-
-            date = '.'.join((day, mounth, year))
-            aired = '-'.join((year, mounth, day))
 
             info = {
                 'video': {
                     'title': title,
-                    'plot': description,
-                    'aired': aired,
-                    'date': date,
+                    # 'plot': description,
+                    # 'aired': aired,
+                    # 'date': date,
                     'duration': duration,
-                    'year': year,
+                    # 'year': year,
                     # 'genre': category,
                     'mediatype': 'tvshow'
                 }
@@ -234,7 +146,7 @@ def list_videos(params):
                     action='download_video',
                     module_path=params.module_path,
                     module_name=params.module_name,
-                    id=id) + ')'
+                    video_url=video_url) + ')'
             )
             context_menu = []
             context_menu.append(download_video)
@@ -248,7 +160,7 @@ def list_videos(params):
                     module_name=params.module_name,
                     action='replay_entry',
                     next='play_r',
-                    id=id
+                    video_url=video_url
                 ),
                 'is_playable': True,
                 'info': info,
@@ -256,33 +168,28 @@ def list_videos(params):
             })
 
         # More videos...
-        videos.append({
-            'label': common.ADDON.get_localized_string(30700),
-            'url': common.PLUGIN.get_url(
-                module_path=params.module_path,
-                module_name=params.module_name,
-                action='replay_entry',
-                category_url=params.category_url,
-                category_name=params.category_name,
-                next='list_videos',
-                page=str(int(params.page) + 1),
-                update_listing=True,
-                previous_listing=str(videos)
-            )
-        })
+        # videos.append({
+        #     'label': common.ADDON.get_localized_string(30700),
+        #     'url': common.PLUGIN.get_url(
+        #         module_path=params.module_path,
+        #         module_name=params.module_name,
+        #         action='replay_entry',
+        #         category_url=params.category_url,
+        #         category_name=params.category_name,
+        #         next='list_videos',
+        #         page=str(int(params.page) + 1),
+        #         update_listing=True,
+        #         previous_listing=str(videos)
+        #     )
+        # })
 
     return common.PLUGIN.create_listing(
         videos,
         sort_methods=(
-            common.sp.xbmcplugin.SORT_METHOD_DATE,
-            common.sp.xbmcplugin.SORT_METHOD_DURATION,
-            common.sp.xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE,
-            common.sp.xbmcplugin.SORT_METHOD_GENRE,
-            common.sp.xbmcplugin.SORT_METHOD_PLAYCOUNT,
             common.sp.xbmcplugin.SORT_METHOD_UNSORTED
         ),
         content='tvshows',
-        update_listing='update_listing' in params,
+        # update_listing='update_listing' in params,
         category=common.get_window_title(params)
     )
 
@@ -311,14 +218,15 @@ def start_live_tv_stream(params):
 def get_video_url(params):
     """Get video URL and start video player"""
     if params.next == 'play_r' or params.next == 'download_video':
-        file_video = utils.get_webcontent(
-            URL_INFO_CONTENT % (params.id)
-        )
-        media_json = json.loads(file_video)
-        stream_url = ''
-        for media in media_json:
-            if media['ID'] == params.id:
-                stream_url = media['MEDIA']['VIDEOS']['HLS'].encode('utf-8')
-        return stream_url
+        file_video = utils.get_webcontent(params.video_url)
+        video_id = re.compile(
+            r'dailymotion.com/embed/video/(.*?)[\"\?]').findall(
+                file_video)[0]
+        if params.next == 'download_video':
+            return resolver.get_stream_dailymotion(
+                video_id, True)
+        else:
+            return resolver.get_stream_dailymotion(
+                video_id, False)
     elif params.next == 'play_l':
         return params.url
