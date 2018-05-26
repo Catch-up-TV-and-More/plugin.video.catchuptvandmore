f7ae8b69ccdeaa0ec299af247729510b98397fba
Marc Roy <wark@free.fr>
2018-05-26 12:31:58 +0900

    Add Ina - Search Button

--- resources/lib/root/websites/ina.py
+++ resources/lib/root/websites/ina.py
@@ -31,7 +31,7 @@ from resources.lib import common
 
 # TO DO
 # Add Premium Account (purchase an account to test)
-# Add a search button / last videos
+# Add last videos
 
 URL_ROOT = 'http://www.ina.fr'
 
@@ -40,6 +40,9 @@ URL_PROGRAMS = URL_ROOT + '/blocs/rubrique_sommaire/196?order=asc&page=%s&nbResu
 
 URL_VIDEOS = URL_ROOT + '/layout/set/ajax/recherche/result?q=%s&autopromote=0&typeBlock=ina_resultat_exalead&s=date_diffusion&sa=0&b=%s&type=Video&r=&hf=48&c=ina_emission'
 # Name Program, Nb Video (+ 48)
+
+URL_VIDEOS_SEARCH = URL_ROOT + '/layout/set/ajax/recherche/result?q=%s&autopromote=&b=%s&type=Video&r=&hf=48'
+# Query, Nb Video (+ 48)
 
 URL_STREAM = 'https://player.ina.fr/notices/%s'
 # VideoId
@@ -54,6 +57,8 @@ def website_entry(params):
         return list_videos(params)
     elif 'play' in params.next:
         return get_video_url(params)
+    elif 'search' in params.next:
+        return search(params)
     return None
 
 CATEGORIES = {
@@ -80,6 +85,19 @@ def root(params):
                 window_title=category_name
             )
         })
+    
+    # Search videos
+    modes.append({
+        'label': common.GETTEXT('Search videos'),
+        'url': common.PLUGIN.get_url(
+            module_path=params.module_path,
+            module_name=params.module_name,
+            action='website_entry',
+            next='search',
+            window_title=common.GETTEXT('Search videos'),
+            is_folder=False
+        )
+    })
 
     return common.PLUGIN.create_listing(
         modes,
@@ -261,6 +279,105 @@ def list_videos(params):
             )
         })
 
+    elif params.next == 'list_videos_search':
+    
+        replay_episodes_json = utils.get_webcontent(
+            URL_VIDEOS_SEARCH % (params.query, params.nb_videos))
+        list_episodes_jsonparser = json.loads(replay_episodes_json)
+        list_episodes_soup = bs(list_episodes_jsonparser["content"], 'html.parser')
+        list_episodes = list_episodes_soup.find_all(
+            'div', class_='media zoomarticle')
+
+        for episode in list_episodes:
+            if episode.find(
+                'div', class_=re.compile("media-inapremium-search")):
+                video_title = '[Ina Premium] ' + episode.find(
+                    'img').get('alt').encode('utf-8')
+            else:
+                video_title = episode.find(
+                    'img').get('alt').encode('utf-8')
+            video_id = episode.find('a').get('href').split('/')[2]
+            video_img = episode.find(
+                'img').get('src')
+            video_duration_text_datas = episode.find('span', class_='duration').get_text().split(' ')
+            video_duration = 0
+            for video_duration_datas in video_duration_text_datas:
+                if 's' in video_duration_datas:
+                    video_duration_datas = video_duration_datas.replace('s', '')
+                    video_duration = video_duration + int(video_duration_datas)
+                elif 'm' in video_duration_datas:
+                    video_duration_datas = video_duration_datas.replace('m', '')
+                    video_duration = video_duration + (int(video_duration_datas) * 60)
+                elif 'h' in video_duration_datas:
+                    video_duration_datas = video_duration_datas.replace('h', '')
+                    video_duration = video_duration + (int(video_duration_datas) * 3600)
+
+            
+            if episode.find('span', class_='broadcast'):
+                video_date = episode.find('span', class_='broadcast').get_text().split('/')
+                day = video_date[0]
+                mounth = video_date[1]
+                year = video_date[2]
+            else:
+                day = '00'
+                mounth = '00'
+                year = '0000'
+            date = '.'.join((day, mounth, year))
+            aired = '-'.join((year, mounth, day))
+
+            info = {
+                'video': {
+                    'title': video_title,
+                    'aired': aired,
+                    'date': date,
+                    'duration': video_duration,
+                    # 'plot': video_plot,
+                    'year': year,
+                    'mediatype': 'tvshow'
+                }
+            }
+
+            download_video = (
+                common.GETTEXT('Download'),
+                'XBMC.RunPlugin(' + common.PLUGIN.get_url(
+                    action='download_video',
+                    module_path=params.module_path,
+                    module_name=params.module_name,
+                    video_id=video_id) + ')'
+            )
+            context_menu = []
+            context_menu.append(download_video)
+
+            videos.append({
+                'label': video_title,
+                'thumb': video_img,
+                'url': common.PLUGIN.get_url(
+                    module_path=params.module_path,
+                    module_name=params.module_name,
+                    action='website_entry',
+                    next='play_r',
+                    video_id=video_id
+                ),
+                'is_playable': True,
+                'info': info,
+                'context_menu': context_menu
+            })
+
+        # More videos...
+        videos.append({
+            'label': '# ' + common.ADDON.get_localized_string(30700),
+            'url': common.PLUGIN.get_url(
+                module_path=params.module_path,
+                module_name=params.module_name,
+                action='website_entry',
+                next='list_videos_1',
+                query=params.query,
+                nb_videos=str(int(params.nb_videos) + 48),
+                update_listing=True,
+                previous_listing=str(videos)
+            )
+        })
+
     return common.PLUGIN.create_listing(
         videos,
         sort_methods=(
@@ -283,3 +400,18 @@ def get_video_url(params):
             if child.tag == '{http://search.yahoo.com/mrss/}content':
                 stream_url = child.attrib['url']
     return stream_url
+
+def search(params):
+    keyboard = common.sp.xbmc.Keyboard(
+        default='',
+        title='',
+        hidden=False)
+    keyboard.doModal()
+    if keyboard.isConfirmed():
+        query = keyboard.getText()
+        params['nb_videos'] = '0'
+        params['query'] = query
+        params['next'] = 'list_videos_search'
+        return list_videos(params)
+    else:
+        return None
