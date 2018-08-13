fb71272b721fe65ea034032bb3aef1d0a2087af5
wwark <wark@free.fr>
2018-08-13 22:24:12 +0200

    [Update] Quest OD Replay

--- resources/lib/root/channels/uk/questod.py
+++ resources/lib/root/channels/uk/questod.py
@@ -27,18 +27,26 @@ from resources.lib import resolver
 from resources.lib import common
 
 # TO DO
-#
+# Add Show A-Z Replay
+# Live TV protected by DRM
 
 URL_ROOT = 'https://www.questod.co.uk'
 
-URL_SHOWS = URL_ROOT + '/api/shows/?limit=12&page=%s'
-# page
+URL_SHOWS = URL_ROOT + '/api/shows/%s/?limit=12&page=%s'
+# mode, page
 
 URL_VIDEOS = URL_ROOT + '/api/show-detail/%s'
 # showId
 
 URL_STREAM = URL_ROOT + '/api/video-playback/%s'
 # videoId
+
+SHOW_MODE = {
+    'FEATURED': 'featured',
+    'MOST POPULAR': 'most-popular',
+    'NEW': 'new',
+    # 'A-Z': 'a-z'
+}
 
 def channel_entry(params):
     """Entry function of the module"""
@@ -63,8 +71,26 @@ def list_shows(params):
 
     if params.next == 'list_shows_1':
 
+        for show_mode_title, show_mode_value in SHOW_MODE.iteritems():
+
+            shows.append({
+                'label': show_mode_title,
+                'url': common.PLUGIN.get_url(
+                    module_path=params.module_path,
+                    module_name=params.module_name,
+                    action='replay_entry',
+                    page='1',
+                    show_mode_value=show_mode_value,
+                    next='list_shows_2',
+                    show_title=show_mode_title,
+                    window_title=show_mode_title
+                )
+            })
+
+    elif params.next == 'list_shows_2':
+
         list_shows_json = utils.get_webcontent(
-            URL_SHOWS % params.page)
+            URL_SHOWS % (params.show_mode_value, params.page))
         list_shows_jsonparser = json.loads(list_shows_json)
 
         for show_datas in list_shows_jsonparser["items"]:
@@ -80,7 +106,7 @@ def list_shows(params):
                     module_path=params.module_path,
                     module_name=params.module_name,
                     action='replay_entry',
-                    next='list_shows_2',
+                    next='list_shows_3',
                     show_id=show_id,
                     show_title=show_title,
                     window_title=show_title
@@ -96,12 +122,13 @@ def list_shows(params):
                 action='replay_entry',
                 next='list_shows_1',
                 page=str(int(params.page) + 1),
+                show_mode_value=params.show_mode_value,
                 update_listing=True,
                 previous_listing=str(shows)
             )
         })
 
-    elif params.next == 'list_shows_2':
+    elif params.next == 'list_shows_3':
 
         list_seasons_json = utils.get_webcontent(
             URL_VIDEOS % params.show_id)
@@ -147,53 +174,55 @@ def list_videos(params):
             URL_VIDEOS % params.show_id)
         list_videos_jsonparser = json.loads(list_videos_json)
 
-        for video_datas in list_videos_jsonparser["videos"]["episode"][params.show_season_id]:
+        if 'episode' in list_videos_jsonparser["videos"]:
+            if params.show_season_id in list_videos_jsonparser["videos"]["episode"]:
+                for video_datas in list_videos_jsonparser["videos"]["episode"][params.show_season_id]:
 
-            video_title = video_datas["title"]
+                    video_title = video_datas["title"]
 
-            video_duration = video_datas["videoDuration"]
-            video_plot = video_datas["description"]
-            video_img = video_datas["image"]["src"]
-            video_id = video_datas["id"]
+                    video_duration = video_datas["videoDuration"]
+                    video_plot = video_datas["description"]
+                    video_img = video_datas["image"]["src"]
+                    video_id = video_datas["id"]
 
-            info = {
-                'video': {
-                    'title': video_title,
-                    # 'aired': aired,
-                    # 'date': date,
-                    'duration': video_duration,
-                    'plot': video_plot,
-                    # 'year': year,
-                    'mediatype': 'tvshow'
-                }
-            }
+                    info = {
+                        'video': {
+                            'title': video_title,
+                            # 'aired': aired,
+                            # 'date': date,
+                            'duration': video_duration,
+                            'plot': video_plot,
+                            # 'year': year,
+                            'mediatype': 'tvshow'
+                        }
+                    }
 
-            download_video = (
-                common.GETTEXT('Download'),
-                'XBMC.RunPlugin(' + common.PLUGIN.get_url(
-                    action='download_video',
-                    module_path=params.module_path,
-                    module_name=params.module_name,
-                    video_id=video_id) + ')'
-            )
-            context_menu = []
-            context_menu.append(download_video)
+                    download_video = (
+                        common.GETTEXT('Download'),
+                        'XBMC.RunPlugin(' + common.PLUGIN.get_url(
+                            action='download_video',
+                            module_path=params.module_path,
+                            module_name=params.module_name,
+                            video_id=video_id) + ')'
+                    )
+                    context_menu = []
+                    context_menu.append(download_video)
 
-            videos.append({
-                'label': video_title,
-                'thumb': video_img,
-                'fanart': video_img,
-                'url': common.PLUGIN.get_url(
-                    module_path=params.module_path,
-                    module_name=params.module_name,
-                    action='replay_entry',
-                    next='play_r',
-                    video_id=video_id
-                ),
-                'is_playable': True,
-                'info': info,
-                'context_menu': context_menu
-            })
+                    videos.append({
+                        'label': video_title,
+                        'thumb': video_img,
+                        'fanart': video_img,
+                        'url': common.PLUGIN.get_url(
+                            module_path=params.module_path,
+                            module_name=params.module_name,
+                            action='replay_entry',
+                            next='play_r',
+                            video_id=video_id
+                        ),
+                        'is_playable': True,
+                        'info': info,
+                        'context_menu': context_menu
+                    })
 
     return common.PLUGIN.create_listing(
         videos,
@@ -218,7 +247,11 @@ def get_video_url(params):
         list_stream_jsonparser = json.loads(list_stream_json)
 
         if 'errors' in list_stream_jsonparser:
-            utils.send_notification(
-                common.ADDON.get_localized_string(30716))
+            if list_stream_jsonparser["errors"][0]["status"] == '403':
+                utils.send_notification(
+                    common.ADDON.get_localized_string(30713))
+            else:
+                utils.send_notification(
+                    common.ADDON.get_localized_string(30716))
             return None
         return list_stream_jsonparser["playback"]["streamUrlHls"]
