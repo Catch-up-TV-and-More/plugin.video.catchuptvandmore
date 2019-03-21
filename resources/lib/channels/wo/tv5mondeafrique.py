fdf6f804fba164f32d9138966dfcb17b5a548cdd
Marc Roy <wark@free.fr>
2019-03-21 18:40:31 +0100

    0.2.5~beta12 [Remove] Beautifulsoup - [ViàChannels] (FR)

--- resources/lib/channels/wo/tv5mondeafrique.py
+++ resources/lib/channels/wo/tv5mondeafrique.py
@@ -31,17 +31,12 @@ from resources.lib.labels import LABELS
 from resources.lib import web_utils
 from resources.lib import download
 
-
-from bs4 import BeautifulSoup as bs
-
 import json
 import re
 import urlquick
 
-
 # TO DO
-# Download Mode / QUality Mode
-
+# QUality Mode
 
 URL_TV5MAF_ROOT = 'https://afrique.tv5monde.com'
 
@@ -63,12 +58,11 @@ def list_categories(plugin, item_id):
     - ...
     """
     resp = urlquick.get(URL_TV5MAF_ROOT + '/videos')
-    root_soup = bs(resp.text, 'html.parser')
-    list_categories_datas = root_soup.find_all(
-        'h2', class_='tv5-title tv5-title--beta u-color--goblin')
-    for category_datas in list_categories_datas:
-        category_title = category_datas.find('a').text
-        category_url = URL_TV5MAF_ROOT + category_datas.find('a').get('href')
+    root = resp.parse()
+
+    for category_datas in root.iterfind(".//h2[@class='tv5-title tv5-title--beta u-color--goblin'"):
+        category_title = category_datas.find('.//a').text
+        category_url = URL_TV5MAF_ROOT + category_datas.find('.//a').get('href')
         item = Listitem()
         item.label = category_title
         item.set_callback(
@@ -86,16 +80,15 @@ def list_programs(plugin, item_id, category_url):
     - ...
     """
     resp = urlquick.get(category_url)
-    root_soup = bs(resp.text, 'html.parser')
-    list_programs_datas = root_soup.find_all(
-        'div', class_='grid-col-12 grid-col-m-4')
-    for program_datas in list_programs_datas:
-        program_title = program_datas.find('h2').get_text().strip()
-        program_url = URL_TV5MAF_ROOT + program_datas.find('a').get('href')
-        if 'http' in program_datas.find('img').get('src'):
-            program_image = program_datas.find('img').get('src')
+    root = resp.parse()
+
+    for program_datas in root.iterfind(".//div[@class='grid-col-12 grid-col-m-4'"):
+        program_title = program_datas.find('.//h2').text.strip()
+        program_url = URL_TV5MAF_ROOT + program_datas.find('.//a').get('href')
+        if 'http' in program_datas.find('.//img').get('src'):
+            program_image = program_datas.find('.//img').get('src')
         else:
-            program_image = URL_TV5MAF_ROOT + program_datas.find('img').get('src')
+            program_image = URL_TV5MAF_ROOT + program_datas.find('.//img').get('src')
 
         item = Listitem()
         item.label = program_title
@@ -110,12 +103,12 @@ def list_programs(plugin, item_id, category_url):
 def list_videos(plugin, item_id, program_url):
 
     resp = urlquick.get(program_url)
-    root_soup = bs(resp.text, 'html.parser')
-    if root_soup.find('div', class_='u-bg--concrete u-pad-t--l u-pad-b--l') is None:
-        video_datas = root_soup.find('div', class_='tv5-player')
-        video_title = video_datas.find('h1').text.strip()
+    root = resp.parse()
+    if root.find(".//div[@class='u-bg--concrete u-pad-t--l u-pad-b--l']") is None:
+        video_datas = root.find(".//div[@class='tv5-player']")
+        video_title = video_datas.find('.//h1').text.strip()
         video_image = re.compile(r'image\" content=\"(.*?)\"').findall(resp.text)[0]
-        video_plot = video_datas.find('div', class_='tv5-desc to-expand u-mg-t--m u-mg-b--s').text.strip()
+        video_plot = video_datas.find(".//div[@class='tv5-desc to-expand u-mg-t--m u-mg-b--s']").text.strip()
 
         item = Listitem()
         item.label = video_title
@@ -137,11 +130,11 @@ def list_videos(plugin, item_id, program_url):
 
         yield item
     else:
-        list_seasons = root_soup.find(
-            'div', class_='tv5-pagerTop tv5-pagerTop--green').find_all('a')
+        list_seasons = root.find(
+            ".//div[@class='tv5-pagerTop tv5-pagerTop--green']").findall('.//a')
         if len(list_seasons) > 1:
             for season in list_seasons:
-                season_title = 'Saison ' + season.get_text()
+                season_title = 'Saison ' + season.text
                 season_url = URL_TV5MAF_ROOT + season.get('href')
 
                 item = Listitem()
@@ -152,13 +145,13 @@ def list_videos(plugin, item_id, program_url):
                     season_url=season_url)
                 yield item
         else:
-            list_videos_datas = root_soup.find(
-                'div', class_='u-bg--concrete u-pad-t--l u-pad-b--l').find_all(
-                    'div', class_='grid-col-12 grid-col-m-4')
+            list_videos_datas = root.find(
+                ".//div[@class='u-bg--concrete u-pad-t--l u-pad-b--l']").findall(
+                    ".//div[@class='grid-col-12 grid-col-m-4']")
             for video_datas in list_videos_datas:
-                video_title = video_datas.find('h2').text.strip()
-                video_image = video_datas.find('img').get('src')
-                video_url = URL_TV5MAF_ROOT + video_datas.find('a').get('href')
+                video_title = video_datas.find('.//h2').text.strip()
+                video_image = video_datas.find('.//img').get('src')
+                video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get('href')
 
                 item = Listitem()
                 item.label = video_title
@@ -182,14 +175,12 @@ def list_videos(plugin, item_id, program_url):
 @Route.register
 def list_videos_season(plugin, item_id, season_url):
     resp = urlquick.get(season_url)
-    root_soup = bs(resp.text, 'html.parser')
-    list_videos_datas = root_soup.find(
-        'div', class_='u-bg--concrete u-pad-t--l u-pad-b--l').find_all(
-            'div', class_='grid-col-12 grid-col-m-4')
-    for video_datas in list_videos_datas:
-        video_title = video_datas.find('h2').text.strip()
-        video_image = video_datas.find('img').get('src')
-        video_url = URL_TV5MAF_ROOT + video_datas.find('a').get('href')
+    root = resp.parse("div", attrs={"class": "u-bg--concrete u-pad-t--l u-pad-b--l"})
+
+    for video_datas in root.iterfind(".//div[@class='grid-col-12 grid-col-m-4']"):
+        video_title = video_datas.find('.//h2').text.strip()
+        video_image = video_datas.find('.//img').get('src')
+        video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get('href')
 
         item = Listitem()
         item.label = video_title
