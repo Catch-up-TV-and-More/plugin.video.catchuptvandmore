41830020fe9bafe7360fa40ab11d672343265c9c
Wark <wark@free.fr>
2019-04-09 18:20:29 +0200

    [favourites] Add PL

--- resources/lib/channels/pl/tvp.py
+++ resources/lib/channels/pl/tvp.py
@@ -29,6 +29,7 @@ from codequick import Route, Resolver, Listitem, utils, Script
 
 from resources.lib.labels import LABELS
 from resources.lib import web_utils
+from resources.lib.listitem_utils import item_post_treatment, item2dict
 
 import re
 import urlquick
@@ -64,12 +65,12 @@ LIVE_TVP3_REGIONS = {
 }
 
 
-def live_entry(plugin, item_id, item_dict):
+def live_entry(plugin, item_id, item_dict, **kwargs):
     return get_live_url(plugin, item_id, item_id.upper(), item_dict)
 
 
 @Resolver.register
-def get_live_url(plugin, item_id, video_id, item_dict):
+def get_live_url(plugin, item_id, video_id, item_dict, **kwargs):
     final_language = Script.setting['tvp3.language']
 
     # If we come from the M3U file and the language
