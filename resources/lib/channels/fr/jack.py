1ca48e13ba929074639bc5de7557d7b704d9348d
Wark <wark@free.fr>
2019-04-17 21:29:48 +0200

    [Fix] Catch-up TV - Jack (mycanal) (FR)

--- resources/lib/channels/fr/jack.py
+++ resources/lib/channels/fr/jack.py
@@ -43,6 +43,7 @@ URL_ROOT = 'https://jack.canal.fr'
 # ChannelName
 
 URL_VIDEOS = URL_ROOT + '/video'
+
 # PageId
 
 
@@ -74,9 +75,8 @@ def list_programs(plugin, item_id, **kwargs):
 
                 item = Listitem()
                 item.label = program_title
-                item.set_callback(list_videos,
-                                  item_id=item_id,
-                                  program_title=program_title)
+                item.set_callback(
+                    list_videos, item_id=item_id, program_title=program_title)
                 item_post_treatment(item)
                 yield item
 
@@ -97,15 +97,20 @@ def list_videos(plugin, item_id, program_title, **kwargs):
                     for video_datas in program_datas["content"]["articles"]:
                         if 'video' in video_datas:
                             video_title = video_datas["title"]
-                            video_image = video_datas["mainMedia"]["links"][0][
-                                "href"]
-                            video_image = video_image.replace(
-                                '{width}', '800').replace('{height}', '450')
+                            video_image = ''
+                            if 'links' in video_datas["mainMedia"]["default"]:
+                                video_image = video_datas["mainMedia"][
+                                    "default"]["links"][0]["href"]
+                                video_image = video_image.replace(
+                                    '{width}', '800').replace(
+                                        '{height}', '450')
                             video_plot = video_datas["abstract"]
                             video_id = video_datas["video"]["id"]
                             video_source = video_datas["video"]["source"]
-                            date_value = video_datas["publishedAt"].split(
-                                'T')[0]
+                            date_value = video_datas["publishedAt"].split('T')[
+                                0]
+
+                            print 'video_image url : ' + repr(video_image)
 
                             item = Listitem()
                             item.label = video_title
@@ -113,15 +118,15 @@ def list_videos(plugin, item_id, program_title, **kwargs):
                             item.info['plot'] = video_plot
                             item.info.date(date_value, '%Y-%m-%d')
 
-                            item.set_callback(get_video_url,
-                                              item_id=item_id,
-                                              video_id=video_id,
-                                              video_label=LABELS[item_id] +
-                                              ' - ' + item.label,
-                                              video_source=video_source)
-                            item_post_treatment(item,
-                                                is_playable=True,
-                                                is_downloadable=True)
+                            item.set_callback(
+                                get_video_url,
+                                item_id=item_id,
+                                video_id=video_id,
+                                video_label=LABELS[item_id] + ' - ' +
+                                item.label,
+                                video_source=video_source)
+                            item_post_treatment(
+                                item, is_playable=True, is_downloadable=True)
                             yield item
 
 
@@ -138,6 +143,5 @@ def get_video_url(plugin,
         return resolver_proxy.get_stream_youtube(plugin, video_id,
                                                  download_mode, video_label)
     elif 'dailymotion' in video_source:
-        return resolver_proxy.get_stream_dailymotion(plugin, video_id,
-                                                     download_mode,
-                                                     video_label)
+        return resolver_proxy.get_stream_dailymotion(
+            plugin, video_id, download_mode, video_label)
