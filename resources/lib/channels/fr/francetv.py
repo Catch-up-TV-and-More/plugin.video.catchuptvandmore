639be2a455d937623acad92719134380a9ab1324
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-11-15 08:31:22 +0100

    [fix] France TV zip_longest import

--- resources/lib/channels/fr/francetv.py
+++ resources/lib/channels/fr/francetv.py
@@ -39,9 +39,9 @@ import time
 import urlquick
 
 try:
-    from itertools import zip_longest as zip_longest
-except Exception:
-    from itertools import zip_longest as zip_longest
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
 """
 Channels:
     * France 2
