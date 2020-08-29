5d1b7295eb4bc21bc62c58d11ecb26351ad15b92
Wark <wark@free.fr>
2020-08-29 07:33:12 +0200

    [BE - TV Lux] Fix Live TV

--- plugin.video.catchuptvandmore/resources/lib/channels/be/tvlux.py
+++ plugin.video.catchuptvandmore/resources/lib/channels/be/tvlux.py
@@ -146,4 +146,9 @@ def get_video_url(plugin,
 def get_live_url(plugin, item_id, **kwargs):
 
     resp = urlquick.get(URL_LIVE, max_age=-1)
-    return re.compile(r'"sourceURL":"(.*?)"').findall(resp.text)[0]
+    root = resp.parse()
+    live_datas = root.findall('.//iframe')[0].get('src')
+
+    resp2 = urlquick.get(live_datas, max_age=-1)
+    return re.compile(
+        r'file\"\:\"(.*?)\"').findall(resp2.text)[0]
