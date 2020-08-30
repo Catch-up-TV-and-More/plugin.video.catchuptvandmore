51a7c89703d4a3b7c318b8f5b73ae868eedf3d26
Wark <wark@free.fr>
2020-08-30 08:27:11 +0200

    Update spdx for new files

--- plugin.video.catchuptvandmore/resources/lib/channels/es/rtve.py
+++ plugin.video.catchuptvandmore/resources/lib/channels/es/rtve.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-    Catch-up TV & More
-    Copyright (C) 2018  SylvainCecchetto
-
+    Copyright (C) 2016-2020 Team Catch-up TV & More
     This file is part of Catch-up TV & More.
-
-    Catch-up TV & More is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    Catch-up TV & More is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with Catch-up TV & More; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    SPDX-License-Identifier: GPL-2.0-or-later
 """
 
 # The unicode_literals import only has
@@ -54,19 +39,20 @@ def list_categories(plugin, item_id, **kwargs):
 
     for category_datas in root.iterfind(".//h2"):
         category_title = category_datas.find('.//a').get('title').replace('ver todos los programas de ', '')
-        category_url = category_datas.find('.//a').get('href')
+        category_url = category_datas.find('.//a').get('href').replace('/1/', '/\%/')
 
         item = Listitem()
         item.label = category_title
-        item.set_callback(list_sub_categories,
+        item.set_callback(list_programs,
                           item_id=item_id,
-                          category_url=category_url)
+                          category_url=category_url,
+                          page='1')
         item_post_treatment(item)
         yield item
 
 
 @Route.register
-def list_sub_categories(plugin, item_id, category_url, **kwargs):
+def list_programs(plugin, item_id, category_url, **kwargs):
     """
     - ...
     """
