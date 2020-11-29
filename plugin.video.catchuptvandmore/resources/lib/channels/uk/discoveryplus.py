2c9571c6cacd84944839483314e0debb56abe2d9
Wark <wark@free.fr>
2020-11-29 15:17:44 +0100

    First work for adding discovery+

--- plugin.video.catchuptvandmore/resources/lib/channels/uk/discoveryplus.py
+++ plugin.video.catchuptvandmore/resources/lib/channels/uk/discoveryplus.py
@@ -34,32 +34,13 @@ from kodi_six import xbmcgui
 
 URL_ROOT = 'https://www.discoveryplus.co.uk'
 
-URL_SHOWS = URL_ROOT + '/api/shows/%s'
+URL_API = 'https://disco-api.discoveryplus.co.uk'
+
+URL_CATEGORIES = URL_API + '/cms/routes/channel/%s'
 # mode
 
-URL_SHOWS_AZ = URL_ROOT + '/api/shows%s'
-# mode
-
-URL_VIDEOS = URL_ROOT + '/api/show-detail/%s'
-# showId
-
-URL_STREAM = URL_ROOT + '/api/video-playback/%s'
-# path
-
-URL_LIVE = URL_ROOT + '/api/channel-playback/%s'
-
-URL_LICENCE_KEY = 'https://lic.caas.conax.com/nep/wv/license|Content-Type=&User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3041.0 Safari/537.36&PreAuthorization=%s&Host=lic.caas.conax.com|R{SSM}|'
+# URL_LICENCE_KEY = 'https://lic.caas.conax.com/nep/wv/license|Content-Type=&User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3041.0 Safari/537.36&PreAuthorization=%s&Host=lic.caas.conax.com|R{SSM}|'
 # videoId
-
-CATEGORIES_MODE = {
-    'FEATURED': 'featured',
-    'MOST POPULAR': 'most-popular',
-    'NEW': 'new',
-    'LEAVING SOON': 'last-chance',
-    'Box Sets': 'box-set'
-}
-
-CATEGORIES_MODE_AZ = {'A-Z': '-az'}
 
 
 @Route.register
@@ -67,7 +48,6 @@ def discoveryplus_root(plugin, **kwargs):
 
     # (item_id, label, thumb, fanart)
     channels = [
-        ('all', 'ALL', 'all.png', 'all_fanart.jpg'),
         ('quest', 'Quest', 'questtv.png', 'questtv_fanart.jpg'),
         ('really', 'Really', 'really.png', 'really_fanart.jpg'),
         ('quest-red', 'Quest Red', 'questred.png', 'questred_fanart.jpg'),
@@ -95,84 +75,64 @@ def list_categories(plugin, item_id, **kwargs):
     - Informations
     - ...
     """
-    for category_name_title, category_name_value in list(CATEGORIES_MODE.items(
-    )):
-
-        item = Listitem()
-        item.label = category_name_title
-        item.set_callback(list_programs_mode,
-                          item_id=item_id,
-                          category_name_value=category_name_value)
-        item_post_treatment(item)
-        yield item
-
-    for category_name_title, category_name_value in list(CATEGORIES_MODE_AZ.items(
-    )):
-
-        item = Listitem()
-        item.label = category_name_title
-        item.set_callback(list_programs_mode_az,
-                          item_id=item_id,
-                          category_name_value=category_name_value)
-        item_post_treatment(item)
-        yield item
-
-
-@Route.register
-def list_programs_mode(plugin, item_id, category_name_value, **kwargs):
-    """
-    Build programs listing
-    - Les feux de l'amour
-    - ...
-    """
-    if item_id == 'all':
-        resp = urlquick.get(URL_SHOWS % category_name_value)
-    else:
-        resp = urlquick.get(URL_SHOWS % category_name_value + '?channel=%s' % item_id)
+    payload = {'include': 'default', 'decorators': 'viewingHistory'}
+    headers = {
+        'origin': 'https://www.discoveryplus.co.uk',
+        'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
+        'referer': 'https://www.discoveryplus.co.uk/',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'accept-language': 'en-US,en;q=0.9'
+    }
+    resp = urlquick.get(URL_CATEGORIES % item_id,
+                        # params=payload,
+                        json=payload,
+                        headers=headers)
+    # Return HTTP 400 :'(
     json_parser = json.loads(resp.text)
 
-    for program_datas in json_parser["items"]:
-        program_title = program_datas["title"]
-        program_id = program_datas["id"]
-        program_image = ''
-        if 'image' in program_datas:
-            program_image = program_datas["image"]["src"]
-
-        item = Listitem()
-        item.label = program_title
-        item.art['thumb'] = item.art['landscape'] = program_image
-        item.set_callback(list_program_seasons,
-                          item_id=item_id,
-                          program_id=program_id)
-        item_post_treatment(item)
-        yield item
-
-
-@Route.register
-def list_programs_mode_az(plugin, item_id, category_name_value, **kwargs):
-    """
-    Build programs listing
-    - Les feux de l'amour
-    - ...
-    """
-    if item_id == 'all':
-        resp = urlquick.get(URL_SHOWS_AZ % category_name_value)
-    else:
-        resp = urlquick.get(URL_SHOWS_AZ % category_name_value + '?channel=%s' % item_id)
-    json_parser = json.loads(resp.text)
-
-    for program_datas_letter in json_parser["items"]:
-        for program_datas in program_datas_letter["items"]:
-            program_title = program_datas["title"]
-            program_id = program_datas["id"]
+    for category_datas in json_parser["items"]:
+        if 'collection' in category_datas['type']:
+            category_title = category_datas['attributes']['title']
 
             item = Listitem()
-            item.label = program_title
-            item.set_callback(list_program_seasons,
-                              item_id=item_id,
-                              program_id=program_id)
+            item.label = category_title
+            item.set_callback(list_programs,
+                            item_id=item_id,
+                            category_title=category_title)
             item_post_treatment(item)
             yield item
+
+
+@Route.register
+def list_programs(plugin, item_id, category_title, **kwargs):
+    """
+    Build programs listing
+    - Les feux de l'amour
+    - ...
+    """
+    return False
+    # if item_id == 'all':
+    #     resp = urlquick.get(URL_SHOWS % category_title)
+    # else:
+    #     resp = urlquick.get(URL_SHOWS % category_title + '?channel=%s' % item_id)
+    # json_parser = json.loads(resp.text)
+
+    # for program_datas in json_parser["items"]:
+    #     program_title = program_datas["title"]
+    #     program_id = program_datas["id"]
+    #     program_image = ''
+    #     if 'image' in program_datas:
+    #         program_image = program_datas["image"]["src"]
+
+    #     item = Listitem()
+    #     item.label = program_title
+    #     item.art['thumb'] = item.art['landscape'] = program_image
+    #     item.set_callback(list_program_seasons,
+    #                       item_id=item_id,
+    #                       program_id=program_id)
+    #     item_post_treatment(item)
+    #     yield item
 
 
 @Route.register
@@ -182,60 +142,62 @@ def list_program_seasons(plugin, item_id, program_id, **kwargs):
     - Season 1
     - ...
     """
-    resp = urlquick.get(URL_VIDEOS % program_id)
-    json_parser = json.loads(resp.text)
+    return False
+    # resp = urlquick.get(URL_VIDEOS % program_id)
+    # json_parser = json.loads(resp.text)
 
-    for program_season_datas in json_parser["show"]["seasonNumbers"]:
-        program_season_name = 'Season - ' + str(program_season_datas)
-        program_season_number = program_season_datas
+    # for program_season_datas in json_parser["show"]["seasonNumbers"]:
+    #     program_season_name = 'Season - ' + str(program_season_datas)
+    #     program_season_number = program_season_datas
 
-        item = Listitem()
-        item.label = program_season_name
-        item.set_callback(list_videos,
-                          item_id=item_id,
-                          program_id=program_id,
-                          program_season_number=program_season_number)
-        item_post_treatment(item)
-        yield item
+    #     item = Listitem()
+    #     item.label = program_season_name
+    #     item.set_callback(list_videos,
+    #                       item_id=item_id,
+    #                       program_id=program_id,
+    #                       program_season_number=program_season_number)
+    #     item_post_treatment(item)
+    #     yield item
 
 
 @Route.register
 def list_videos(plugin, item_id, program_id, program_season_number, **kwargs):
 
-    resp = urlquick.get(URL_VIDEOS % program_id)
-    json_parser = json.loads(resp.text)
+    return False
+    # resp = urlquick.get(URL_VIDEOS % program_id)
+    # json_parser = json.loads(resp.text)
 
-    at_least_one_item = False
+    # at_least_one_item = False
 
-    if 'episode' in json_parser["videos"]:
-        if str(program_season_number) in json_parser["videos"]["episode"]:
-            for video_datas in json_parser["videos"]["episode"][str(
-                    program_season_number)]:
-                at_least_one_item = True
-                video_title = video_datas["title"]
-                video_duration = old_div(int(video_datas["videoDuration"]), 1000)
-                video_plot = video_datas["description"]
-                video_image = video_datas["image"]["src"]
-                video_id = video_datas["path"]
+    # if 'episode' in json_parser["videos"]:
+    #     if str(program_season_number) in json_parser["videos"]["episode"]:
+    #         for video_datas in json_parser["videos"]["episode"][str(
+    #                 program_season_number)]:
+    #             at_least_one_item = True
+    #             video_title = video_datas["title"]
+    #             video_duration = old_div(int(video_datas["videoDuration"]), 1000)
+    #             video_plot = video_datas["description"]
+    #             video_image = video_datas["image"]["src"]
+    #             video_id = video_datas["path"]
 
-                item = Listitem()
-                item.label = video_title
-                item.art['thumb'] = item.art['landscape'] = video_image
-                item.art['fanart'] = video_image
-                item.info["plot"] = video_plot
-                item.info["duration"] = video_duration
+    #             item = Listitem()
+    #             item.label = video_title
+    #             item.art['thumb'] = item.art['landscape'] = video_image
+    #             item.art['fanart'] = video_image
+    #             item.info["plot"] = video_plot
+    #             item.info["duration"] = video_duration
 
-                item.set_callback(get_video_url,
-                                  item_id=item_id,
-                                  video_id=video_id)
-                item_post_treatment(item,
-                                    is_playable=True,
-                                    is_downloadable=True)
-                yield item
+    #             item.set_callback(get_video_url,
+    #                               item_id=item_id,
+    #                               video_id=video_id)
+    #             item_post_treatment(item,
+    #                                 is_playable=True,
+    #                                 is_downloadable=True)
+    #             yield item
 
-    if not at_least_one_item:
-        plugin.notify(plugin.localize(30718), '')
-        yield False
+    # if not at_least_one_item:
+    #     plugin.notify(plugin.localize(30718), '')
+    #     yield False
 
 
 @Resolver.register
@@ -245,86 +207,88 @@ def get_video_url(plugin,
                   download_mode=False,
                   **kwargs):
 
-    resp = urlquick.get(URL_STREAM % video_id, max_age=-1)
-    json_parser = json.loads(resp.text)
+    return False
+    # resp = urlquick.get(URL_STREAM % video_id, max_age=-1)
+    # json_parser = json.loads(resp.text)
 
-    if 'error' in json_parser:
-        if json_parser["error"] is not None:
-            if json_parser["error"]["status"] == '403':
-                plugin.notify('ERROR', plugin.localize(30713))
-            else:
-                plugin.notify('ERROR', plugin.localize(30716))
-            return False
+    # if 'error' in json_parser:
+    #     if json_parser["error"] is not None:
+    #         if json_parser["error"]["status"] == '403':
+    #             plugin.notify('ERROR', plugin.localize(30713))
+    #         else:
+    #             plugin.notify('ERROR', plugin.localize(30716))
+    #         return False
 
-    if 'drmToken' in json_parser["playback"]:
+    # if 'drmToken' in json_parser["playback"]:
 
-        if get_kodi_version() < 18:
-            xbmcgui.Dialog().ok('Info', plugin.localize(30602))
-            return False
+    #     if get_kodi_version() < 18:
+    #         xbmcgui.Dialog().ok('Info', plugin.localize(30602))
+    #         return False
 
-        is_helper = inputstreamhelper.Helper('mpd', drm='widevine')
-        if not is_helper.check_inputstream():
-            return False
+    #     is_helper = inputstreamhelper.Helper('mpd', drm='widevine')
+    #     if not is_helper.check_inputstream():
+    #         return False
 
-        if download_mode:
-            xbmcgui.Dialog().ok('Info', plugin.localize(30603))
-            return False
+    #     if download_mode:
+    #         xbmcgui.Dialog().ok('Info', plugin.localize(30603))
+    #         return False
 
-        token = json_parser["playback"]["drmToken"]
+    #     token = json_parser["playback"]["drmToken"]
 
-        item = Listitem()
-        item.path = json_parser["playback"]["streamUrlDash"]
-        item.label = get_selected_item_label()
-        item.art.update(get_selected_item_art())
-        item.info.update(get_selected_item_info())
-        item.property[INPUTSTREAM_PROP] = 'inputstream.adaptive'
-        item.property['inputstream.adaptive.manifest_type'] = 'mpd'
-        item.property[
-            'inputstream.adaptive.license_type'] = 'com.widevine.alpha'
-        item.property[
-            'inputstream.adaptive.license_key'] = URL_LICENCE_KEY % token
+    #     item = Listitem()
+    #     item.path = json_parser["playback"]["streamUrlDash"]
+    #     item.label = get_selected_item_label()
+    #     item.art.update(get_selected_item_art())
+    #     item.info.update(get_selected_item_info())
+    #     item.property[INPUTSTREAM_PROP] = 'inputstream.adaptive'
+    #     item.property['inputstream.adaptive.manifest_type'] = 'mpd'
+    #     item.property[
+    #         'inputstream.adaptive.license_type'] = 'com.widevine.alpha'
+    #     item.property[
+    #         'inputstream.adaptive.license_key'] = URL_LICENCE_KEY % token
 
-        return item
-    else:
-        final_video_url = json_parser["playback"]["streamUrlHls"]
+    #     return item
+    # else:
+    #     final_video_url = json_parser["playback"]["streamUrlHls"]
 
-        if download_mode:
-            return download.download_video(final_video_url)
+    #     if download_mode:
+    #         return download.download_video(final_video_url)
 
-        return final_video_url
+    #     return final_video_url
 
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
 
-    if get_kodi_version() < 18:
-        xbmcgui.Dialog().ok('Info', plugin.localize(30602))
-        return False
-
-    is_helper = inputstreamhelper.Helper('mpd', drm='widevine')
-    if not is_helper.check_inputstream():
-        return False
-
-    resp = urlquick.get(URL_LIVE % item_id, max_age=-1)
-
-    if len(re.compile(r'drmToken\"\:\"(.*?)\"').findall(resp.text)) > 0:
-        token = re.compile(r'drmToken\"\:\"(.*?)\"').findall(resp.text)[0]
-        if len(re.compile(r'streamUrlDash\"\:\"(.*?)\"').findall(
-                resp.text)) > 0:
-            live_url = re.compile(r'streamUrlDash\"\:\"(.*?)\"').findall(
-                resp.text)[0]
-
-            item = Listitem()
-            item.path = live_url
-            item.label = get_selected_item_label()
-            item.art.update(get_selected_item_art())
-            item.info.update(get_selected_item_info())
-            item.property[INPUTSTREAM_PROP] = 'inputstream.adaptive'
-            item.property['inputstream.adaptive.manifest_type'] = 'mpd'
-            item.property[
-                'inputstream.adaptive.license_type'] = 'com.widevine.alpha'
-            item.property[
-                'inputstream.adaptive.license_key'] = URL_LICENCE_KEY % token
-            return item
-    plugin.notify('ERROR', plugin.localize(30713))
     return False
+    # if get_kodi_version() < 18:
+    #     xbmcgui.Dialog().ok('Info', plugin.localize(30602))
+    #     return False
+
+    # is_helper = inputstreamhelper.Helper('mpd', drm='widevine')
+    # if not is_helper.check_inputstream():
+    #     return False
+
+    # resp = urlquick.get(URL_LIVE % item_id, max_age=-1)
+
+    # if len(re.compile(r'drmToken\"\:\"(.*?)\"').findall(resp.text)) > 0:
+    #     token = re.compile(r'drmToken\"\:\"(.*?)\"').findall(resp.text)[0]
+    #     if len(re.compile(r'streamUrlDash\"\:\"(.*?)\"').findall(
+    #             resp.text)) > 0:
+    #         live_url = re.compile(r'streamUrlDash\"\:\"(.*?)\"').findall(
+    #             resp.text)[0]
+
+    #         item = Listitem()
+    #         item.path = live_url
+    #         item.label = get_selected_item_label()
+    #         item.art.update(get_selected_item_art())
+    #         item.info.update(get_selected_item_info())
+    #         item.property[INPUTSTREAM_PROP] = 'inputstream.adaptive'
+    #         item.property['inputstream.adaptive.manifest_type'] = 'mpd'
+    #         item.property[
+    #             'inputstream.adaptive.license_type'] = 'com.widevine.alpha'
+    #         item.property[
+    #             'inputstream.adaptive.license_key'] = URL_LICENCE_KEY % token
+    #         return item
+    # plugin.notify('ERROR', plugin.localize(30713))
+    # return False
