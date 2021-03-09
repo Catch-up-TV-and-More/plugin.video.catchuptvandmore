1351e24aafc1608f13fbbb7f205806140ddbdd2e
Wark <wark@free.fr>
2021-03-09 11:50:28 +0100

    [France24 - WO] Fix Live TV and Catch-up TV

--- resources/lib/channels/wo/france24.py
+++ resources/lib/channels/wo/france24.py
@@ -24,6 +24,7 @@ def root_catchup_tv(plugin, item_id, **kwargs):
     # http://apis.france24.com/products/get_product/78dcf358-9333-4fb2-a035-7b91e9705b13?token_application=66b85dad-3ad5-40f3-ab32-2305fc2357ea
     root_json_url = 'products/get_product/78dcf358-9333-4fb2-a035-7b91e9705b13'
     root_json_r = urlquick.get(URL_API(root_json_url),
+                               headers={'User-Agent': web_utils.get_random_ua()},
                                params={'token_application': TOKEN_APP})
     json_root = json.loads(root_json_r.text)
 
@@ -310,6 +311,7 @@ def get_live_url(plugin, item_id, **kwargs):
 
     root_json_url = 'products/get_product/78dcf358-9333-4fb2-a035-7b91e9705b13'
     root_json_r = urlquick.get(URL_API(root_json_url),
+                               headers={'User-Agent': web_utils.get_random_ua()},
                                params={'token_application': TOKEN_APP})
     json_root = json.loads(root_json_r.text)
 
