a3e948eb75a0920b2fd29ae0d12ccee3731a4cd2
Wark <wark@free.fr>
2021-03-09 17:50:29 +0100

    [TV5MondeAfrique - WO] Fix Catch-up TV

--- resources/lib/channels/wo/tv5mondeafrique.py
+++ resources/lib/channels/wo/tv5mondeafrique.py
@@ -30,21 +30,22 @@ def list_categories(plugin, item_id, **kwargs):
     - Informations
     - ...
     """
-    resp = urlquick.get(URL_TV5MAF_ROOT + '/videos')
+    resp = urlquick.get(URL_TV5MAF_ROOT + '/videos',
+                        headers={'User-Agent': web_utils.get_random_ua()})
     root = resp.parse()
 
-    for category_datas in root.iterfind(
-            ".//h2[@class='tv5-title tv5-title--beta u-color--goblin']"):
-        category_title = category_datas.find('.//a').text
-        category_url = URL_TV5MAF_ROOT + category_datas.find('.//a').get(
-            'href')
-        item = Listitem()
-        item.label = category_title
-        item.set_callback(list_programs,
-                          item_id=item_id,
-                          category_url=category_url)
-        item_post_treatment(item)
-        yield item
+    for category_datas in root.iterfind(".//div[@class='view-header']"):
+        if category_datas.find(".//h2") is not None:
+            category_title = category_datas.find(".//h2").text
+            if category_datas.find('.//a') is not None:
+                category_url = URL_TV5MAF_ROOT + category_datas.find('.//a').get('href')
+                item = Listitem()
+                item.label = category_title
+                item.set_callback(list_programs,
+                                  item_id=item_id,
+                                  category_url=category_url)
+                item_post_treatment(item)
+                yield item
 
 
 @Route.register
@@ -54,47 +55,48 @@ def list_programs(plugin, item_id, category_url, **kwargs):
     - Les feux de l'amour
     - ...
     """
-    resp = urlquick.get(category_url)
+    resp = urlquick.get(category_url,
+                        headers={'User-Agent': web_utils.get_random_ua()})
     root = resp.parse()
 
-    for program_datas in root.iterfind(
-            ".//div[@class='grid-col-12 grid-col-m-4']"):
-        program_title = program_datas.find('.//h2/span').text.strip()
-        program_url = URL_TV5MAF_ROOT + program_datas.find('.//a').get('href')
-        if 'http' in program_datas.find('.//img').get('src'):
-            program_image = program_datas.find('.//img').get('src')
-        else:
-            program_image = URL_TV5MAF_ROOT + program_datas.find('.//img').get(
-                'src')
+    for program_datas in root.iterfind(".//div[@class='views-row']"):
+        if program_datas.find('.//h2') is not None:
+            program_title = program_datas.find('.//h2').text
+            if 'http' in program_datas.find('.//a').get('href'):
+                program_url = program_datas.find('.//a').get('href')
+            else:
+                program_url = URL_TV5MAF_ROOT + program_datas.find('.//a').get('href')
+            if 'http' in program_datas.find('.//img').get('src'):
+                program_image = program_datas.find('.//img').get('src')
+            else:
+                program_image = URL_TV5MAF_ROOT + program_datas.find('.//img').get(
+                    'src')
 
-        item = Listitem()
-        item.label = program_title
-        item.art['thumb'] = item.art['landscape'] = program_image
-        item.set_callback(list_videos,
-                          item_id=item_id,
-                          program_url=program_url)
-        item_post_treatment(item)
-        yield item
+            item = Listitem()
+            item.label = program_title
+            item.art['thumb'] = item.art['landscape'] = program_image
+            item.set_callback(list_videos,
+                              item_id=item_id,
+                              program_url=program_url)
+            item_post_treatment(item)
+            yield item
 
 
 @Route.register
 def list_videos(plugin, item_id, program_url, **kwargs):
 
-    resp = urlquick.get(program_url)
+    resp = urlquick.get(program_url,
+                        headers={'User-Agent': web_utils.get_random_ua()})
     root = resp.parse()
-    if root.find(".//div[@class='tv5-pagerTop tv5-pagerTop--green']") is None:
-        video_datas = root.find(".//div[@class='tv5-player']")
+    if root.find(".//nav[@class='title-season']") is None:
+        video_datas = root.find(".//main[@class='layout-3col__full']")
         video_title = video_datas.find('.//h1').text.strip()
         video_image = re.compile(r'image\" content=\"(.*?)\"').findall(
             resp.text)[0]
-        video_plot = video_datas.find(
-            ".//div[@class='tv5-desc to-expand u-mg-t--m u-mg-b--s']").get(
-                'data-originaltext')
 
         item = Listitem()
         item.label = video_title
         item.art['thumb'] = item.art['landscape'] = video_image
-        item.info['plot'] = video_plot
 
         item.set_callback(get_video_url,
                           item_id=item_id,
@@ -104,7 +106,7 @@ def list_videos(plugin, item_id, program_url, **kwargs):
         yield item
     else:
         list_seasons = root.find(
-            ".//div[@class='tv5-pagerTop tv5-pagerTop--green']").findall(
+            ".//nav[@class='title-season']").findall(
                 './/a')
         if len(list_seasons) > 1:
             for season in list_seasons:
@@ -120,46 +122,65 @@ def list_videos(plugin, item_id, program_url, **kwargs):
                 yield item
         else:
             list_videos_datas = root.findall(
-                ".//div[@class='grid-col-12 grid-col-m-4']")
+                ".//div[@class='season-1 views-row']") # TODO Get season number
             for video_datas in list_videos_datas:
-                video_title = video_datas.find('.//h2/span').text.strip()
-                video_image = video_datas.find('.//img').get('src')
-                video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get(
-                    'href')
+                if video_datas.find('.//h2') is not None:
+                    video_title = video_datas.find('.//h2').text
+                    if 'http' in video_datas.find('.//img').get('src'):
+                        video_image = video_datas.find('.//img').get('src')
+                    else:
+                        video_image = URL_TV5MAF_ROOT + video_datas.find('.//img').get(
+                            'src')
+                    if 'http' in video_datas.find('.//a').get('href'):
+                        video_url = video_datas.find('.//a').get('href')
+                    else:
+                        video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get(
+                            'href')
 
-                item = Listitem()
-                item.label = video_title
-                item.art['thumb'] = item.art['landscape'] = video_image
+                    item = Listitem()
+                    item.label = video_title
+                    item.art['thumb'] = item.art['landscape'] = video_image
 
-                item.set_callback(get_video_url,
-                                  item_id=item_id,
-                                  video_url=video_url)
-                item_post_treatment(item,
-                                    is_playable=True,
-                                    is_downloadable=True)
-                yield item
+                    item.set_callback(get_video_url,
+                                      item_id=item_id,
+                                      video_url=video_url)
+                    item_post_treatment(item,
+                                        is_playable=True,
+                                        is_downloadable=True)
+                    yield item
 
 
 @Route.register
 def list_videos_season(plugin, item_id, season_url, **kwargs):
-    resp = urlquick.get(season_url)
+    resp = urlquick.get(season_url,
+                        headers={'User-Agent': web_utils.get_random_ua()})
     root = resp.parse()
 
     for video_datas in root.iterfind(
-            ".//div[@class='grid-col-12 grid-col-m-4']"):
-        video_title = video_datas.find('.//h2/span').text.strip()
-        video_image = video_datas.find('.//img').get('src')
-        video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get('href')
+            ".//div[@class='season-1 views-row']"): # TODO
+        if video_datas.find('.//h2') is not None:
+            video_title = video_datas.find('.//h2').text
+            if 'http' in video_datas.find('.//img').get('src'):
+                video_image = video_datas.find('.//img').get('src')
+            else:
+                video_image = URL_TV5MAF_ROOT + video_datas.find('.//img').get(
+                    'src')
+            if 'http' in video_datas.find('.//a').get('href'):
+                video_url = video_datas.find('.//a').get('href')
+            else:
+                video_url = URL_TV5MAF_ROOT + video_datas.find('.//a').get(
+                    'href')
 
-        item = Listitem()
-        item.label = video_title
-        item.art['thumb'] = item.art['landscape'] = video_image
 
-        item.set_callback(get_video_url,
-                          item_id=item_id,
-                          video_url=video_url)
-        item_post_treatment(item, is_playable=True, is_downloadable=True)
-        yield item
+            item = Listitem()
+            item.label = video_title
+            item.art['thumb'] = item.art['landscape'] = video_image
+
+            item.set_callback(get_video_url,
+                              item_id=item_id,
+                              video_url=video_url)
+            item_post_treatment(item, is_playable=True, is_downloadable=True)
+            yield item
 
 
 @Resolver.register
