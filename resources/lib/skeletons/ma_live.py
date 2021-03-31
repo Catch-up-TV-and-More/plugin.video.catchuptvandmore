cd736492a082c85ab0c32482879b9f76f42adccc
chamchenko <chamchenko@gmail.com>
2021-03-31 02:03:38 +0100

    Add M24, Medi1 and Télé Maroc

--- resources/lib/skeletons/ma_live.py
+++ resources/lib/skeletons/ma_live.py
@@ -20,7 +20,7 @@ menu = {
     'alAoula': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Al Aoula',
-        'thumb': 'channels/ma/alaoula_live.png',
+        'thumb': 'channels/ma/alaoula.png',
         'fanart': 'channels/ma/alaoula_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -29,7 +29,7 @@ menu = {
     'laayoune': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Al Aoula Laayoune',
-        'thumb': 'channels/ma/laayoune_live.png',
+        'thumb': 'channels/ma/laayoune.png',
         'fanart': 'channels/ma/laayoune_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -38,7 +38,7 @@ menu = {
     'arryadia': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Arryadia',
-        'thumb': 'channels/ma/arryadia_live.png',
+        'thumb': 'channels/ma/arryadia.png',
         'fanart': 'channels/ma/arryadia_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -47,7 +47,7 @@ menu = {
     'athaqafia': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Athaqafia',
-        'thumb': 'channels/ma/athaqafia_live.png',
+        'thumb': 'channels/ma/athaqafia.png',
         'fanart': 'channels/ma/athaqafia_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -56,7 +56,7 @@ menu = {
     'alMaghribia': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Al Maghribia',
-        'thumb': 'channels/ma/almaghribia_live.png',
+        'thumb': 'channels/ma/almaghribia.png',
         'fanart': 'channels/ma/almaghribia_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -65,7 +65,7 @@ menu = {
     'assadissa': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Assadissa',
-        'thumb': 'channels/ma/assadissa_live.png',
+        'thumb': 'channels/ma/assadissa.png',
         'fanart': 'channels/ma/assadissa_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
@@ -74,10 +74,50 @@ menu = {
     'tamazight': {
         'resolver': '/resources/lib/channels/ma/snrt:get_live_url',
         'label': 'Tamazight',
-        'thumb': 'channels/ma/tamazight_live.png',
+        'thumb': 'channels/ma/tamazight.png',
         'fanart': 'channels/ma/tamazight_live_fanart.jpg',
         'm3u_group': 'Maroc',
         'enabled': True,
         'order': 7,
+    },
+    'télémaroc': {
+        'resolver': '/resources/lib/channels/ma/telemaroc:get_live_url',
+        'label': 'Télé Maroc',
+        'thumb': 'channels/ma/telemaroc.png',
+        'fanart': 'channels/ma/telemaroc_fanart.jpg',
+        'enabled': True,
+        'order': 8
+    },
+    'Maghreb': {
+        'resolver': '/resources/lib/channels/ma/medi1:get_live_url',
+        'label': 'Medi1 Maghreb',
+        'thumb': 'channels/ma/medi1.png',
+        'fanart': 'channels/ma/medi1maghreb_fanart.jpg',
+        'enabled': True,
+        'order': 9
+    },
+    'Arabic': {
+        'resolver': '/resources/lib/channels/ma/medi1:get_live_url',
+        'label': 'Medi1 Arabic',
+        'thumb': 'channels/ma/medi1.png',
+        'fanart': 'channels/ma/medi1arabic_fanart.jpg',
+        'enabled': True,
+        'order': 10
+    },
+    'Afrique': {
+        'resolver': '/resources/lib/channels/ma/medi1:get_live_url',
+        'label': 'Medi1 Afrique',
+        'thumb': 'channels/ma/medi1afrique.png',
+        'fanart': 'channels/ma/medi1afrique_fanart.jpg',
+        'enabled': True,
+        'order': 11
+    },
+    'm24': {
+        'resolver': '/resources/lib/channels/ma/m24:get_live_url',
+        'label': 'M24',
+        'thumb': 'channels/ma/m24.png',
+        'fanart': 'channels/ma/m24_fanart.jpg',
+        'enabled': True,
+        'order': 12
     }
 }
