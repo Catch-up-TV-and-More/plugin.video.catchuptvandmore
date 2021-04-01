d6118cb81e475b69b30a9eab07a49c4a85a5da02
Wark <wark@free.fr>
2021-04-01 08:53:23 +0200

    More fixes

--- resources/lib/channels/ma/telemaroc.py
+++ resources/lib/channels/ma/telemaroc.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2017, SylvainCecchetto
 # GNU General Public License v2.0+ (see LICENSE.txt or https://www.gnu.org/licenses/gpl-2.0.txt)
@@ -20,9 +21,9 @@ def get_live_url(plugin, item_id, **kwargs):
 
     resp = urlquick.get(URL_LIVES)
     accout_id = re.compile(
-		r'\<iframe.*accounts\/(.*)\/events').findall(resp.text)[0]
+        r'\<iframe.*accounts\/(.*)\/events').findall(resp.text)[0]
     event_id = re.compile(
-		r'\<iframe.*events\/(.*)\/player').findall(resp.text)[0]
+        r'\<iframe.*events\/(.*)\/player').findall(resp.text)[0]
     resp2 = urlquick.get(STREAM_INFO_URL % (accout_id, event_id))
     json_parser = json.loads(resp2.text)
     return json_parser['secure_m3u8_url']
