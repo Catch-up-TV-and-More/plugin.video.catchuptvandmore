9ae86e84c549f4789d4d0682c107f421abcc729f
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2021-11-26 21:11:02 +0100

    [FR - CNEWS] Try hardcoded live id in case of error (#580)

--- resources/lib/channels/fr/cnews.py
+++ resources/lib/channels/fr/cnews.py
@@ -153,7 +153,13 @@ def get_live_url(plugin, item_id, **kwargs):
     root = urlquick.get(URL_LIVE_CNEWS,
                         headers={'User-Agent': web_utils.get_random_ua()},
                         max_age=-1).parse()
-    live_id = root.find(".//div[@id='player_live']").get('data-videoid')
+    try:
+        live_id = root.find(".//div[@id='player_live']").get('data-videoid')
+    except Exception:
+        try:
+            live_id = root.find(".//div[@id='player_live']").get('data-video-id')
+        except Exception:
+            live_id = 'x3b68jn'
     # live_id = re.compile(r'video_id\"\:\"(.*?)[\?\"]',
     #                      re.DOTALL).findall(resp.text)[0]
     return resolver_proxy.get_stream_dailymotion(plugin, live_id, False)
