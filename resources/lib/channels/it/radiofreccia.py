36acda52a4a56cc5edff0b03663b7fe8417c615e
joaopa <jeremielapuree@yahoo.fr>
2022-03-07 11:41:59 -1000

    [IT - RADIOFRECCIA] Enable multiple resolutions

--- resources/lib/channels/it/radiofreccia.py
+++ resources/lib/channels/it/radiofreccia.py
@@ -8,8 +8,14 @@ from __future__ import unicode_literals
 
 import json
 import re
-
+import urlquick
+import inputstreamhelper
+from codequick import Listitem, Resolver
 import sys
+from resources.lib.kodi_utils import (INPUTSTREAM_PROP, get_selected_item_art,
+                                      get_selected_item_info,
+                                      get_selected_item_label)
+
 
 if sys.version_info.major >= 3 and sys.version_info.minor >= 4:
     import html as html_parser
@@ -22,8 +28,6 @@ else:
 
     html_parser = HTMLParser.HTMLParser()
 
-import urlquick
-from codequick import Resolver
 
 PATTERN = re.compile(r'data-media-object="(.*?)"')
 URL_LIVE = "https://play.rtl.it/live/17/radiofreccia-radiovisione/"
@@ -38,4 +42,13 @@ def get_live_url(plugin, item_id, **kwargs):
     media_object = html_parser.unescape(media_objects[0])
     json_media_object = json.loads(media_object)
 
-    return json_media_object['mediaInfo']['uri']
+    is_helper = inputstreamhelper.Helper("mpd")
+    if not is_helper.check_inputstream():
+        return False
+
+    item = Listitem()
+    item.path = json_media_object['mediaInfo']['descriptor'][1]["uri"]
+    item.property[INPUTSTREAM_PROP] = "inputstream.adaptive"
+    item.property["inputstream.adaptive.manifest_type"] = "mpd"
+
+    return item
