a87e37d749746b7a9feec5e39b1b26367d021efd
joaopa <jeremielapuree@yahoo.fr>
2022-03-24 19:51:20 -1000

    [FR - WEO] Fix live

--- resources/lib/channels/fr/weo.py
+++ resources/lib/channels/fr/weo.py
@@ -7,10 +7,15 @@
 from __future__ import unicode_literals
 import re
 
-from codequick import Resolver
+from codequick import Listitem, Resolver
 import urlquick
 
+import inputstreamhelper
 from resources.lib import resolver_proxy, web_utils
+from resources.lib.kodi_utils import (INPUTSTREAM_PROP, get_selected_item_art,
+                                      get_selected_item_info,
+                                      get_selected_item_label)
+from resources.lib.menu_utils import item_post_treatment
 
 
 # TODO
@@ -24,9 +29,22 @@ URL_LIVE = URL_ROOT + "/direct/"
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
 
-    resp = urlquick.get(
-        URL_LIVE, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
-    live_id = re.compile(r'dailymotion.com/embed/video/(.*?)[\?\"]').findall(resp.text)[0]
-    return resolver_proxy.get_stream_dailymotion(plugin,
-                                                 live_id,
-                                                 False)
+    resp = urlquick.get(URL_LIVE, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
+    for possibility in resp.parse().findall('.//iframe'):
+        if possibility.get('allowfullscreen'):
+            video_page = 'https:' + possibility.get('src')
+
+    # In a perfect world, digiteka extractor would not be broken in youtube-dl
+    resp2 = urlquick.get(video_page, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
+    video_url = re.compile(r'live\"\:\{\"src\"\:\"(.*?)\"').findall(resp2.text)[0].replace("\/", "/")
+
+    is_helper = inputstreamhelper.Helper("hls")
+    if not is_helper.check_inputstream():
+        return False
+
+    item = Listitem()
+    item.path = video_url
+    item.property[INPUTSTREAM_PROP] = "inputstream.adaptive"
+    item.property["inputstream.adaptive.manifest_type"] = "hls"
+
+    return item
