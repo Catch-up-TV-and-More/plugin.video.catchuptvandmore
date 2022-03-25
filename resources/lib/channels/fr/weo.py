d70a68443852a433061a197439af3dfc96faf0de
darodi <4682830+darodi@users.noreply.github.com>
2022-03-25 19:35:54 +0100

    [FR - WEO] Fix live
    - resolver_proxy.py : make __get_non_ia_stream_with_quality private again
    - regexp correction, string replace correction
    - fix local variable 'video_page' might be referenced before assignment
    - fix flake8

--- resources/lib/channels/fr/weo.py
+++ resources/lib/channels/fr/weo.py
@@ -13,7 +13,6 @@ import urlquick
 
 from resources.lib import resolver_proxy, web_utils
 
-
 # TODO
 # Add Replay
 
@@ -21,18 +20,25 @@ URL_ROOT = "https://www.weo.fr"
 
 URL_LIVE = URL_ROOT + "/direct/"
 
+PATTERN_FILE = re.compile(r'live\":{\"src\":\"(.*?)\"')
+
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-
+    video_page = None
     resp = urlquick.get(URL_LIVE, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
     for possibility in resp.parse().findall('.//iframe'):
         if possibility.get('allowfullscreen'):
             video_page = 'https:' + possibility.get('src')
 
+    if video_page is None:
+        return False
+
     # In a perfect world, digiteka extractor would not be broken in youtube-dl
     resp2 = urlquick.get(video_page, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
-    video_url = re.compile(r'live\"\:\{\"src\"\:\"(.*?)\"').findall(resp2.text)[0].replace("\/", "/")
+    found_files = PATTERN_FILE.findall(resp2.text)
+    if len(found_files) == 0:
+        return False
 
-
+    video_url = found_files[0].replace("\\", "")
     return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
