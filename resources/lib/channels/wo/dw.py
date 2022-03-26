80530ff09725860038067af8fc4cc73872224cfc
joaopa <jeremielapuree@yahoo.fr>
2022-03-25 22:00:01 -1000

    [Project] Use get_stream_with_quality in a few more places

--- resources/lib/channels/wo/dw.py
+++ resources/lib/channels/wo/dw.py
@@ -7,8 +7,13 @@
 from __future__ import unicode_literals
 import re
 
-from codequick import Resolver, Script
+from codequick import Script
 import urlquick
+
+from codequick import Listitem, Route
+# noinspection PyUnresolvedReferences
+from codequick import Resolver
+from resources.lib import resolver_proxy, web_utils
 
 
 # TODO
@@ -22,12 +27,12 @@ URL_ROOT = 'http://www.dw.com'
 def get_live_url(plugin, item_id, **kwargs):
     final_language = kwargs.get('language', Script.setting['dw.language'])
 
-    stream_url = ''
+    video_url = ''
     resp = urlquick.get(URL_ROOT + '/%s' % final_language.lower())
     list_lives_datas = re.compile(r'name="file_name" value="(.*?)"').findall(
         resp.text)
 
     for live_datas in list_lives_datas:
         if 'm3u8' in live_datas:
-            stream_url = live_datas
-    return stream_url
+            video_url = live_datas
+    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
