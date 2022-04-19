c6fd0ab5db1506a28fa1771d55f371783977a282
itasli <ilyas.tasli@outlook.fr>
2022-04-19 05:38:06 -1000

    [TR - Tele1] Add channel

--- resources/lib/skeletons/tr_live.py
+++ resources/lib/skeletons/tr_live.py
@@ -113,12 +113,20 @@ menu = {
         'enabled': True,
         'order': 12
     },
+    'tele1': {
+        'resolver': '/resources/lib/channels/tr/tele1:get_live_url',
+        'label': 'Tele 1',
+        'thumb': 'channels/tr/tele1.png',
+        'fanart': 'channels/tr/tele1_fanart.jpg',
+        'enabled': True,
+        'order': 13
+    },
     'tv8': {
         'resolver': '/resources/lib/channels/tr/tv8:get_live_url',
         'label': 'TV 8',
         'thumb': 'channels/tr/tv8.png',
         'fanart': 'channels/tr/tv8_fanart.jpg',
         'enabled': True,
-        'order': 13
+        'order': 14
     },
 }
