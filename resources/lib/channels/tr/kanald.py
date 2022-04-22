5d30e96b12bd0fe6e269094557d21cc46d3d7fdc
itasli <ilyas.tasli@outlook.fr>
2022-04-23 00:58:32 +0200

    [TR - Kanal D] Added custom headers
    J'ai ajouté un user-agent afin de faire fonctionner le live, le lien était correcte mais ne fonctionnait pas sans.

--- resources/lib/channels/tr/kanald.py
+++ resources/lib/channels/tr/kanald.py
@@ -23,5 +23,6 @@ def get_live_url(plugin, item_id, **kwargs):
 
     resp = urlquick.get(URL_LIVE)
     video_url = re.compile('\"contentUrl\":\"https://media.duhnet.tv(.*?)\"').findall(resp.text)[0]
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
 
-    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
+    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls", headers=headers)
