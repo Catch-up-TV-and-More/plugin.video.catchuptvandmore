e2e39e2b63262dfded4b9cadb966ce3687146c5d
joaopa <jeremielapuree@yahoo.fr>
2022-09-30 18:54:28 -1000

    [FR] Use get_stream_with_quality in a few more places

--- resources/lib/channels/fr/tvr.py
+++ resources/lib/channels/fr/tvr.py
@@ -10,7 +10,7 @@ import re
 from codequick import Resolver
 import urlquick
 
-from resources.lib import web_utils
+from resources.lib import resolver_proxy, web_utils
 
 
 # TODO
@@ -24,6 +24,7 @@ URL_LIVE = URL_ROOT + '/direct'
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
 
-    resp = urlquick.get(
-        URL_LIVE, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
-    return re.compile(r'data-source\=\"(.*?)\"').findall(resp.text)[0]
+    resp = urlquick.get(URL_LIVE, headers={"User-Agent": web_utils.get_random_ua()}, max_age=-1)
+    video_url = re.compile(r'data-source\=\"(.*?)\"').findall(resp.text)[0]
+
+    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
