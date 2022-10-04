4774bf77b581d680705fd365d3dd8072f8ae0970
joaopa <jeremielapuree@yahoo.fr>
2022-10-04 09:25:31 -1000

    [PROJECT] Remove the notification of TV Guide download

--- resources/lib/xmltv.py
+++ resources/lib/xmltv.py
@@ -538,10 +538,6 @@ def download_xmltv_file(country_id, date, xmltv_fp):
                 need_to_downlod_xmltv_file = True
 
         if need_to_downlod_xmltv_file:
-            Script.notify(
-                Script.localize(30722),
-                Script.localize(30730),
-                display_time=5000)
             r = urlquick.get(xmltv_url, max_age=-1)
             with open(xmltv_fp, 'wb') as f:
                 f.write(r.content)
@@ -579,10 +575,6 @@ def download_xmltv_file(country_id, date, xmltv_fp):
             date.strftime('%Y-%m-%d'),
             xmltv_ids
         )
-        Script.notify(
-            Script.localize(30722),
-            Script.localize(30730),
-            display_time=5000)
         sd.get_xmltv()
 
     else:
