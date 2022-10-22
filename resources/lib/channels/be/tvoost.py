1adb577910f847ee27dc5d2512b7a69e026c86b3
Peter Notebaert <mail@peno.be>
2022-10-22 23:00:09 +0200

    [BE] Update live links for ATV, ROBTV, TVL and TVOOST

--- resources/lib/channels/be/tvoost.py
+++ resources/lib/channels/be/tvoost.py
@@ -12,4 +12,4 @@ from codequick import Resolver
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
 
-    return 'http://api.new.livestream.com/accounts/27755193/events/8511193/live.m3u8'
+    return 'https://live.zendzend.com/streams/29375_399410/playlist.m3u8'
