eacecbd9de07f069d6d36e9e32b58eb7add423c2
nictjir <63444876+nictjir@users.noreply.github.com>
2023-02-22 18:44:47 +0200

    [UK - My5] Updated Header
    Removed non contributors from header

--- resources/lib/channels/uk/my5.py
+++ resources/lib/channels/uk/my5.py
@@ -2,8 +2,7 @@
 # Copyright: (c) 2022, Joaopa, nictjir  GNU General Public License v2.0+
 # (see LICENSE.txt or https://www.gnu.org/licenses/gpl-2.0.txt)
 # This file is part of Catch-up TV & More
-# with contributions by
-#    berkhornet
+
 
 from __future__ import unicode_literals
 
