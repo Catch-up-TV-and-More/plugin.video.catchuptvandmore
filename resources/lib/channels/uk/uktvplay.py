216d08de050e84acfb6264752bb5f94aedbe9d32
nictjir <63444876+nictjir@users.noreply.github.com>
2024-03-09 16:03:45 -1000

    [UK - UKTVPLAY] Fix replay

--- resources/lib/channels/uk/uktvplay.py
+++ resources/lib/channels/uk/uktvplay.py
@@ -8,16 +8,13 @@ from __future__ import unicode_literals
 from builtins import str
 import json
 import re
-import requests
 
-import inputstreamhelper
 from codequick import Listitem, Resolver, Route
 from kodi_six import xbmcgui
 import urlquick
 
 from resources.lib import resolver_proxy, web_utils
 
-from resources.lib.kodi_utils import get_kodi_version, get_selected_item_art, get_selected_item_label, get_selected_item_info, INPUTSTREAM_PROP
 from resources.lib.menu_utils import item_post_treatment
 
 
@@ -25,13 +22,6 @@ from resources.lib.menu_utils import item_post_treatment
 # Mode code brightcove protected by DRM in resolver_proxy
 
 URL_ROOT = 'https://uktvplay.uktv.co.uk'
-
-URL_BRIGHTCOVE_POLICY_KEY = 'https://players.brightcove.net/%s/%s_default/index.min.js'
-# AccountId, PlayerId
-
-URL_BRIGHTCOVE_VIDEO_JSON = 'https://edge.api.brightcove.com/'\
-                            'playback/v1/accounts/%s/videos/%s'
-# AccountId, VideoId
 
 URL_API = 'https://vschedules.uktv.co.uk'
 
@@ -60,8 +50,6 @@ URL_LIVE = 'https://uktvplay.uktv.co.uk/watch-live/%s'
 URL_STREAM_LIVE = 'https://v2-streams-elb.simplestreamcdn.com/api/live/stream/%s?key=%s&platform=chrome&user=%s'
 # data_channel, key, user
 
-URL_DATA_BRIGHTCOVE = 'https://uktvplay.co.uk/_next/static/chunks/app/(navigation)/shows/[brand]/[series]/[episode]/[videoId]/page-af077c3ba4e5c8fe.js'
-
 URL_CHANNEL_ID = "https://vschedules.uktv.co.uk/vod/now_and_next/"
 
 URL_LIVE_KEY = 'https://mp.simplestream.com/uktv/1.0.4/ss.js'
@@ -74,6 +62,10 @@ URL_LOGIN_TOKEN = 'https://s3-eu-west-1.amazonaws.com/uktv-static/fgprod/play/6f
 URL_LOGIN_MODAL = 'https://uktvplay.uktv.co.uk/account/'
 
 URL_COMPTE_LOGIN = 'https://live.mppglobal.com/api/accounts/authenticate/'
+
+URL_CHUNKS = "https://uktvplay.co.uk/shows/%s/series-%s/episode-%s/%s"
+
+URL_ID = "https://uktvplay.co.uk/_next/"
 
 GENERIC_HEADERS = {"User-Agent": web_utils.get_random_ua()}
 
@@ -167,8 +159,8 @@ def list_letters(plugin, item_id, **kwargs):
 @Route.register
 def list_programs(plugin, item_id, letter_value, **kwargs):
 
-    resp = urlquick.get(URL_PROGRAMS %
-                        (letter_value.replace('0-9', '0'), letter_value))
+    FIXED_URL_PROGRAMS = URL_PROGRAMS % (letter_value.replace('0-9', '0'), letter_value)
+    resp = urlquick.get(FIXED_URL_PROGRAMS, headers=GENERIC_HEADERS, max_age=-1)
     json_parser = json.loads(resp.text)
 
     for program_datas in json_parser:
@@ -191,7 +183,7 @@ def list_programs(plugin, item_id, letter_value, **kwargs):
 @Route.register
 def list_seasons(plugin, item_id, program_slug, **kwargs):
 
-    resp = urlquick.get(URL_INFO_PROGRAM % program_slug)
+    resp = urlquick.get(URL_INFO_PROGRAM % program_slug, headers=GENERIC_HEADERS, max_age=-1)
     json_parser = json.loads(resp.text)
 
     for season_datas in json_parser["series"]:
@@ -208,7 +200,7 @@ def list_seasons(plugin, item_id, program_slug, **kwargs):
 @Route.register
 def list_videos(plugin, item_id, serie_id, **kwargs):
 
-    resp = urlquick.get(URL_VIDEOS % serie_id)
+    resp = urlquick.get(URL_VIDEOS % serie_id, headers=GENERIC_HEADERS, max_age=-1)
     json_parser = json.loads(resp.text)
 
     for video_datas in json_parser["episodes"]:
@@ -219,6 +211,11 @@ def list_videos(plugin, item_id, serie_id, **kwargs):
         video_duration = video_datas["duration"] * 60
         video_id = video_datas["video_id"]
 
+        show_name = URL_CHUNKS % (video_datas["brand_slug"],
+                                  video_datas["series_number"],
+                                  video_datas["episode_number"],
+                                  video_datas["video_id"])
+
         item = Listitem()
         item.label = video_title
         item.art['thumb'] = item.art['landscape'] = video_image
@@ -226,23 +223,20 @@ def list_videos(plugin, item_id, serie_id, **kwargs):
         item.info['duration'] = video_duration
         item.set_callback(get_video_url,
                           item_id=item_id,
-                          data_video_id=video_id)
+                          data_video_id=video_id,
+                          show_name=show_name)
         item_post_treatment(item)
         yield item
 
 
-# BRIGHTCOVE Part
-def get_brightcove_policy_key(data_account, data_player):
-    """Get policy key"""
-    file_js = urlquick.get(URL_BRIGHTCOVE_POLICY_KEY %
-                           (data_account, data_player))
-    return re.compile('policyKey:"(.+?)"').findall(file_js.text)[0]
-
-
 @Resolver.register
-def get_video_url(plugin, item_id, data_video_id, **kwargs):
+def get_video_url(plugin, item_id, data_video_id, show_name, **kwargs):
 
-    resp = urlquick.get(URL_DATA_BRIGHTCOVE, headers=GENERIC_HEADERS, max_age=-1)
+    resp = urlquick.get(show_name, headers=GENERIC_HEADERS, max_age=-1)
+    match = re.search('(static/chunks/app/\(navigation\)/shows/\[brand\]/\[series\]/\[episode\]/\[videoId\]/page)(.+?)."\]', resp.text, re.DOTALL)
+
+    full_url_ids = URL_ID + match.group(1) + match.group(2)
+    resp = urlquick.get(full_url_ids, headers=GENERIC_HEADERS, max_age=-1)
     data_account = re.search('accountId:"(.+?)",', resp.text).group(1)
     data_player = re.search('playerId:"(.+?)",', resp.text).group(1)
 
