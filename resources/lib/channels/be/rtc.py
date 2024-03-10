2885900f4107318fb6cfe12e42947fab0c76c551
jmzambon <jeanmarczambon@gmail.com>
2024-03-09 15:37:13 -1000

    [BE - RTC] Fix replay and live

--- resources/lib/channels/be/rtc.py
+++ resources/lib/channels/be/rtc.py
@@ -12,18 +12,20 @@ from builtins import str
 import json
 import urlquick
 from codequick import Listitem, Resolver, Route
-from resources.lib import resolver_proxy, download
+from resources.lib import resolver_proxy, web_utils, download
 from resources.lib.menu_utils import item_post_treatment
 
 URL_ROOT = 'https://www.rtc.be'
 
 URL_LIVE = URL_ROOT + '/live'
 
-LIVE_PLAYER = 'https://tvlocales-player.freecaster.com/embed/%s.json'
+URL_PLAYER = 'https://tvlocales-player.freecaster.com/embed/%s.json'
 
 URL_VIDEOS = URL_ROOT + '/videos'
 
 URL_EMISSIONS = URL_ROOT + '/emissions'
+
+GENERIC_HEADERS = {'User-Agent': web_utils.get_random_ua()}
 
 
 @Route.register
@@ -53,7 +55,7 @@ def list_categories(plugin, item_id, **kwargs):
 
 @Route.register
 def list_programs(plugin, item_id, **kwargs):
-    resp = urlquick.get(URL_EMISSIONS)
+    resp = urlquick.get(URL_EMISSIONS, headers=GENERIC_HEADERS, max_age=-1)
     root = resp.parse()
 
     for program_datas in root.iterfind(".//div[@class='col-sm-4']"):
@@ -82,7 +84,7 @@ def append_schema(url):
 
 @Route.register
 def list_videos(plugin, item_id, next_url, page, **kwargs):
-    resp = urlquick.get(next_url + '?lim_un=%s' % page)
+    resp = urlquick.get(next_url + '?lim_un=%s' % page, headers=GENERIC_HEADERS, max_age=-1)
     root = resp.parse()
 
     for video_datas in root.iterfind(".//div[@class='col-sm-4']"):
@@ -113,34 +115,30 @@ def get_video_url(plugin,
                   video_url,
                   download_mode=False,
                   **kwargs):
-    resp = urlquick.get(video_url, max_age=-1)
+    resp = urlquick.get(video_url, headers=GENERIC_HEADERS, max_age=-1)
+    root = resp.parse()
 
-    javascript_player_urls = re.compile(r'src="(https://rtc\.fcst\.tv/player/embed/.*?)"').findall(resp.text)
-    stream_url = ''
-    for player_url in javascript_player_urls:
-        javascript_resp = urlquick.get(player_url, max_age=-1)
-        # \"https:\\\/\\\/rtc-vod.freecaster.com\\\/vod\\\/rtc\\\/jkmYRKZBKq-720p.mp4\"
-        stream_data_array = re.compile(r'(https?:[/\\]+rtc-vod\.freecaster\.com.*?/([^/]*?)\.mp4)').findall(
-            javascript_resp.text)
-        for stream_data in stream_data_array:
-            stream_url = stream_data[0].replace("\\", "")
-
-    if stream_url == '':
-        plugin.notify(plugin.localize(30600), plugin.localize(30716))
-        return False
+    video_data = root.findall(".//div[@class='freecaster-player']")[0].get('data-fc-token')
+    resp = urlquick.get(URL_PLAYER % video_data, headers=GENERIC_HEADERS, max_age=-1)
+    json_data = json.loads(resp.text)
+    video_url = json_data['video']['src'][0]['src']
+    try:
+        subtitles = json_data['video']['tracks'][0]['src']
+    except KeyError:
+        subtitles = None
 
     if download_mode:
-        return download.download_video(stream_url)
-    return stream_url
+        return download.download_video(video_url)
+    return resolver_proxy.get_stream_with_quality(plugin, video_url, subtitles=subtitles)
 
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-    resp = urlquick.get(URL_LIVE, max_age=-1)
+    resp = urlquick.get(URL_LIVE, headers=GENERIC_HEADERS, max_age=-1)
     root = resp.parse()
 
-    live_data = root.findall(".//div[@class='freecaster-player']")[0].get('data-fc-token')
-    resp2 = urlquick.get(LIVE_PLAYER % live_data, max_age=-1)
-    video_url = json.loads(resp2.text)['video']['src'][0]['src']
+    live_data = root.findall(".//div[@class='freecaster-player']")[0].get('data-video-id')
+    resp = urlquick.get(URL_PLAYER % live_data, headers=GENERIC_HEADERS, max_age=-1)
+    video_url = json.loads(resp.text)['video']['src'][0]['src']
 
-    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
+    return resolver_proxy.get_stream_with_quality(plugin, video_url)
