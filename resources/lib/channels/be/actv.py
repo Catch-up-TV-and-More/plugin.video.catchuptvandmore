499c53bfa63c62d4e3420e8d97d2c8c6b12a008f
darodi <4682830+darodi@users.noreply.github.com>
2024-04-27 11:14:00 +0200

    [BE - ACTV] Fix live

--- resources/lib/channels/be/actv.py
+++ resources/lib/channels/be/actv.py
@@ -5,17 +5,17 @@
 # This file is part of Catch-up TV & More
 
 from __future__ import unicode_literals
-import re
 
 import json
+import re
+
+import urlquick
 # noinspection PyUnresolvedReferences
 from codequick import Resolver, Route
 # noinspection PyUnresolvedReferences
 from codequick.utils import urljoin_partial
-import urlquick
 
-from resources.lib import resolver_proxy
-from resources.lib.web_utils import get_random_ua
+from resources.lib import resolver_proxy, web_utils
 
 URL_ROOT = 'https://www.antennecentre.tv/'
 URL_LIVE = URL_ROOT + 'direct'
@@ -24,6 +24,8 @@ LIVE_PLAYER = 'https://tvlocales-player.freecaster.com/embed/%s.json'
 
 PATTERN_PLAYER = re.compile(r'actv\.fcst\.tv/player/embed/(.*?)\?')
 PATTERN_STREAM = re.compile(r'file\":\"(.*?)\"')
+
+PATTERN_LIVE_TOKEN = re.compile(r'\"live_token\":\s*\"(.*?)\"')
 
 
 @Route.register
@@ -34,11 +36,22 @@ def list_programs(plugin, item_id, **kwargs):
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-    resp = urlquick.get(URL_LIVE, max_age=-1)
-    root = resp.parse()
+    headers = {
+        "User-Agent": web_utils.get_random_ua(),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "fr-BE,en-US;q=0.7,en;q=0.3",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+        "referrer": URL_ROOT,
+    }
 
-    live_data = root.findall(".//div[@class='freecaster-player']")[0].get('data-fc-token')
-    resp2 = urlquick.get(LIVE_PLAYER % live_data, max_age=-1)
-    video_url = json.loads(resp2.text)['video']['src'][0]['src']
+    resp = urlquick.get(URL_LIVE, headers=headers, max_age=-1)
+    m3u8_array = PATTERN_LIVE_TOKEN.findall(resp.text)
+    if len(m3u8_array) == 0:
+        plugin.notify(plugin.localize(30600), plugin.localize(30716))
+        return False
 
-    return resolver_proxy.get_stream_with_quality(plugin, video_url, manifest_type="hls")
+    resp = urlquick.get(LIVE_PLAYER % m3u8_array[0], max_age=-1)
+    video_url = json.loads(resp.text)['video']['src'][0]['src']
+
+    return resolver_proxy.get_stream_with_quality(plugin, video_url)
