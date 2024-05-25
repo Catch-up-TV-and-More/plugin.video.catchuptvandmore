70063be3d490ac98cd3bd9637c81215ff04fb7b7
darodi <4682830+darodi@users.noreply.github.com>
2024-05-24 18:00:07 -1000

    [FR - TF1PLUS] Avoid Fairplay drm

--- resources/lib/web_utils.py
+++ resources/lib/web_utils.py
@@ -36,15 +36,47 @@ else:
 
     html_parser = HTMLParser.HTMLParser()
 
-user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
-    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+
+# see https://www.whatismybrowser.com/guides/the-latest-user-agent/windows
+windows_user_agents = [
+    # Edge on Windows
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/125.0.0.0 Safari/537.36 Edg/125.0.2535.51',
+    # Chrome on Windows
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    # Firefox on windows
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0',
+    # Vivaldi on Windows
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 '
+    'Safari/537.36 Vivaldi/6.7.3329.35'
 ]
+
+# https://www.whatismybrowser.com/guides/the-latest-user-agent/macos
+mac_user_agents = [
+    # Safari on macOS
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 '
+    'Safari/605.1.15',
+    # Firefox on macOS
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:126.0) Gecko/20100101 Firefox/126.0',
+    # Chrome on macOS
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 '
+    'Safari/537.36',
+    # Vivaldi on macOS
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 '
+    'Safari/537.36 Vivaldi/6.7.3329.35',
+    # Edge on macOS
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 '
+    'Safari/537.36 Edg/125.0.2535.51',
+]
+
+linux_user_agents = [
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
+    '(KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'
+]
+
+user_agents = windows_user_agents + mac_user_agents + linux_user_agents
 
 
 def get_ua():
@@ -63,6 +95,10 @@ def get_random_ua():
         str: Random user agent
     """
     return user_agents[randint(0, len(user_agents) - 1)]
+
+
+def get_random_windows_ua():
+    return windows_user_agents[randint(0, len(windows_user_agents) - 1)]
 
 
 # code adapted from weather.weatherbit.io - Thanks Ronie
