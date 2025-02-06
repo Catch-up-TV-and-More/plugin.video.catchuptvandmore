c612f24ced04144174dd5066cb785a8a936a6745
jeff2900 <23429848+jeff2900@users.noreply.github.com>
2025-02-06 12:19:38 +0100

    [FR - FRANCETV] Fix live error

--- resources/lib/channels/fr/francetv.py
+++ resources/lib/channels/fr/francetv.py
@@ -408,12 +408,11 @@ def get_video_url(plugin,
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
     resp = urlquick.get(URL_ROOT, headers={'User-Agent': web_utils.get_random_windows_ua()}, max_age=-1)
-    link_chunks = re.compile(r'script src="(/_next/static/chunks/6289-.*?\.js)" async').findall(resp.text)
-    url_chunks = URL_ROOT + link_chunks[0]
-
-    resp = urlquick.get(url_chunks, headers={'User-Agent': web_utils.get_random_windows_ua()}, max_age=-1)
-    chunk = re.compile(r'([A-Za-z0-9-"]+?):{label:"(.+?)",playerId:"(\w+?-\w+?-\w+?-\w+?-\w+?)"}').findall(resp.text)
-    for channels_id, channels_label, broadcast_id in chunk:
-        channel_id = channels_id.strip('\"')
-        if item_id == channel_id:
-            return resolver_proxy.get_francetv_live_stream(plugin, broadcast_id)
+    link_chunks = re.compile(r'script src="(/_next/static/chunks/\d+?-\w+?\.js)" async').findall(resp.text)
+    for url_chunks in link_chunks:
+        resp = urlquick.get(URL_ROOT + url_chunks, headers={'User-Agent': web_utils.get_random_windows_ua()}, max_age=-1)
+        chunks = re.compile(r'([A-Za-z0-9-"]+?):{label:"(.+?)",playerId:"(\w+?-\w+?-\w+?-\w+?-\w+?)"}').findall(resp.text)
+        for channels_id, channels_label, broadcast_id in chunks:
+            channel_id = channels_id.strip('\"')
+            if item_id == channel_id:
+                return resolver_proxy.get_francetv_live_stream(plugin, broadcast_id)
