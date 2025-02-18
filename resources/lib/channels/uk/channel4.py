84bf4ce9a11cf50c15f8e6fb24ad4ebb03c3587b
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-02-18 23:07:28 +0000

    Capitalise Category Names
    Small improvement to the category names by capitalising the first latter.

--- resources/lib/channels/uk/channel4.py
+++ resources/lib/channels/uk/channel4.py
@@ -53,7 +53,7 @@ def list_categories(plugin, **kwargs):
                 for d in b['sliceItems']:
                     url_item = d['url'].replace('http', 'https').replace(URL_PROGRAMS, URL_ROOT)
                     item = Listitem()
-                    item.label = url_item.replace('/', ' ').split()[-1]
+                    item.label = url_item.replace('/', ' ').split()[-1].title()
                     item.art["thumb"] = item.art["landscape"] = d['image']['href']
                     item.set_callback(list_programs, url=url_item, offset='0')
                     item_post_treatment(item)
