381669bfc7079fea08c03435b72902efc9eb8c23
jeff2900 <23429848+jeff2900@users.noreply.github.com>
2025-07-21 19:10:45 +0200

    [WO - ARTE] Replay add Search

--- resources/lib/channels/wo/arte.py
+++ resources/lib/channels/wo/arte.py
@@ -25,6 +25,7 @@ URL_ARTE_HOME = 'https://api.arte.tv/api/emac/v4/%s/tv/pages/HOME'
 URL_ARTE_CATEGORIES = 'https://api.arte.tv/api/emac/v4/%s/app/pages/%s'
 URL_ARTE_COLLECTION = 'https://api.arte.tv/api/emac/v4/%s/tv/collections/%s'
 URL_ARTE_PROGRAMS = 'https://api.arte.tv/api/emac/v4/%s/tv/programs/%s'
+URL_ARTE_SEARCH = 'https://api.arte.tv/api/emac/v4/%s/app/pages/SEARCH'
 URL_LIVE_ARTE = 'https://api.arte.tv/api/player/v2/config/%s/LIVE'
 # Langue, ...
 
@@ -50,14 +51,15 @@ def list_categories(plugin, item_id, **kwargs):
     - Informations
     - ...
     """
+    # Search items
+    item = Listitem.search(list_videos_search)
+    item_post_treatment(item)
+    yield item
+
     url = URL_ARTE_HOME % DESIRED_LANGUAGE.lower()
-    return list_zone(plugin, url)
-
-
-@Route.register
-def list_zone(plugin, url):
     response = urlquick.get(url, headers=ARTE_API_HEADERS, max_age=-1)
     json_parser = response.json()
+
     for zone in json_parser['zones']:
         # Avoid empty folders
         if not zone['content']['data']:
@@ -81,6 +83,109 @@ def list_zone(plugin, url):
 
 
 @Route.register
+def list_zone(plugin, url):
+    response = urlquick.get(url, headers=ARTE_API_HEADERS, max_age=-1)
+    json_parser = response.json()
+
+    for zone in json_parser['zones']:
+        # Avoid empty folders
+        if not zone['content']['data']:
+            continue
+        # Avoid infinite loop
+        data_url_dic = zone['content']['data'][0]['url'].split("/")
+        data_url_len = len(data_url_dic)
+        data_url = data_url_dic[data_url_len - 3]
+        zone_url_dic = url.split("/")
+        zone_url_len = len(zone_url_dic)
+        zone_url = zone_url_dic[zone_url_len - 1]
+        if data_url == zone_url:
+            continue
+
+        item = Listitem()
+        item.label = zone['title']
+        item.info['plot'] = zone['description']
+        item.set_callback(list_programs, url=url, zone_id=zone['id'])
+        item_post_treatment(item)
+        yield item
+
+
+@Route.register
+def list_videos_search(plugin, search_query, **kwargs):
+    if search_query is None or len(search_query) == 0:
+        return False
+
+    params = {
+        'query': search_query,
+    }
+    response = urlquick.get(URL_ARTE_SEARCH % DESIRED_LANGUAGE.lower(), params=params, headers=ARTE_API_HEADERS, max_age=-1)
+    json_parser = response.json()
+
+    at_least_one_item = False
+    for zone in json_parser['zones']:
+        for data in zone['content']['data']:
+            item = handle_programs(data)
+            at_least_one_item = True
+            yield item
+
+    if not at_least_one_item:
+        plugin.notify(plugin.localize(30718), '')
+        yield False
+
+
+@Resolver.register
+def handle_programs(data):
+    title = data['title']
+    if 'subtitle' in data and data['subtitle']:
+        title += ' - ' + data['subtitle']
+
+    item = Listitem()
+    item.label = title
+    if 'teaserText' in data:
+        item.info['plot'] = data['teaserText']
+    else:
+        item.info['plot'] = data.get('shortDescription', None)
+
+    if 'mainImage' in data:
+        item.art['thumb'] = item.art["fanart"] = data['mainImage']['url'].replace('__SIZE__', '940x530')
+        item.art['landscape'] = data['mainImage']['url'].replace('__SIZE__', '265x397')
+
+    if 'duration' in data:
+        item.info['duration'] = data['duration']
+
+    try:
+        item.info.date(data['availability']['start'].split('T')[0], '%Y-%m-%d')
+    except Exception:
+        pass
+
+    if data['kind']['code'] in ['SHOWS', 'SHOW']:
+        item.set_callback(get_video_url, video_id=data['programId'])
+        item_post_treatment(item, is_playable=True, is_downloadable=True)
+    else:
+        # Assume it's a folder
+        if data['deeplink'] is None:
+            # No items found
+            item.label = Script.localize(30896)
+        else:
+            deeplink_dic = data['deeplink'].split("/")
+            deeplink_len = len(deeplink_dic)
+            deeplink = deeplink_dic[deeplink_len - 1]
+            if deeplink_dic[deeplink_len - 2] == "emac":
+                item.set_callback(list_zone, url=URL_ARTE_CATEGORIES % (DESIRED_LANGUAGE.lower(), deeplink))
+                item_post_treatment(item)
+            elif deeplink_dic[deeplink_len - 2] == "collection":
+                item.set_callback(list_zone, url=URL_ARTE_COLLECTION % (DESIRED_LANGUAGE.lower(), deeplink))
+                item_post_treatment(item)
+            elif deeplink_dic[deeplink_len - 2] == "program":
+                item.set_callback(list_zone, url=URL_ARTE_PROGRAMS % (DESIRED_LANGUAGE.lower(), deeplink))
+                item_post_treatment(item)
+            else:
+                # No items found
+                item.label = Script.localize(30896)
+
+    return item
+
+
+@Route.register
 def list_programs(plugin, url, zone_id):
     response = urlquick.get(url, headers=ARTE_API_HEADERS, max_age=-1)
     json_parser = response.json()
@@ -90,54 +195,7 @@ def list_programs(plugin, url, zone_id):
             data = zone['content']['data']
             break
     for data in zone['content']['data']:
-        title = data['title']
-        if 'subtitle' in data and data['subtitle']:
-            title += ' - ' + data['subtitle']
-
-        item = Listitem()
-        item.label = title
-        if 'teaserText' in data:
-            item.info['plot'] = data['teaserText']
-        else:
-            item.info['plot'] = data.get('shortDescription', None)
-
-        if 'mainImage' in data:
-            item.art['thumb'] = item.art["fanart"] = data['mainImage']['url'].replace('__SIZE__', '940x530')
-            item.art['landscape'] = data['mainImage']['url'].replace('__SIZE__', '265x397')
-
-        if 'duration' in data:
-            item.info['duration'] = data['duration']
-
-        try:
-            item.info.date(data['availability']['start'].split('T')[0], '%Y-%m-%d')
-        except Exception:
-            pass
-
-        if data['kind']['code'] in ['SHOWS', 'SHOW']:
-            item.set_callback(get_video_url, video_id=data['programId'])
-            item_post_treatment(item, is_playable=True, is_downloadable=True)
-        else:
-            # Assume it's a folder
-            if data['deeplink'] is None:
-                # No items found
-                item.label = Script.localize(30896)
-            else:
-                deeplink_dic = data['deeplink'].split("/")
-                deeplink_len = len(deeplink_dic)
-                deeplink = deeplink_dic[deeplink_len - 1]
-                if deeplink_dic[deeplink_len - 2] == "emac":
-                    item.set_callback(list_zone, url=URL_ARTE_CATEGORIES % (DESIRED_LANGUAGE.lower(), deeplink))
-                    item_post_treatment(item)
-                elif deeplink_dic[deeplink_len - 2] == "collection":
-                    item.set_callback(list_zone, url=URL_ARTE_COLLECTION % (DESIRED_LANGUAGE.lower(), deeplink))
-                    item_post_treatment(item)
-                elif deeplink_dic[deeplink_len - 2] == "program":
-                    item.set_callback(list_zone, url=URL_ARTE_PROGRAMS % (DESIRED_LANGUAGE.lower(), deeplink))
-                    item_post_treatment(item)
-                else:
-                    # No items found
-                    item.label = Script.localize(30896)
-
+        item = handle_programs(data)
         yield item
 
 
