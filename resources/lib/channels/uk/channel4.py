152cb1410e72d5e5f45818ee8e74f05944735c3d
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2025-10-28 16:28:17 +0000

    [UK - CHANNEL4] Expose 1080p live streams

--- resources/lib/channels/uk/channel4.py
+++ resources/lib/channels/uk/channel4.py
@@ -11,7 +11,7 @@ import base64
 import re
 import json
 from builtins import str
-
+import requests
 from codequick import Listitem, Script, Resolver, Route
 import urlquick
 
@@ -204,6 +204,16 @@ def get_live_url(plugin, item_id, **kwargs):
             token = field['streams'][0]['token']
             url = field['streams'][0]['uri']
             break
+
+    # Attempt to expose HD resolutions
+    if url and "manifest_sd.mpd" in url:
+        new_url = url.replace("manifest_sd.mpd", "manifest.mpd")
+        try:
+            response = requests.head(new_url, allow_redirects=True, timeout=5)
+            if response.status_code == 200:
+                url = new_url
+        except Exception:
+            pass
 
     cipher = AES.new(bytes('n9cLieYkqwzNCqvi', 'UTF-8'), AES.MODE_CBC, bytes('odzcU3WdUiXLucVd', 'UTF-8'))
     full_decoded_token = unpad(cipher.decrypt(base64.b64decode(token)), 16, style='pkcs7').decode('UTF-8')
