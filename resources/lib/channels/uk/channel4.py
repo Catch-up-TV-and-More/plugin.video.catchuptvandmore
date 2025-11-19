15d0a44914fdf28fa29b733fa3ef2a61e6979989
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2025-11-19 22:32:16 +0000

    [UK C4] Prioritise webvtt subtitles as they contain colour

--- resources/lib/channels/uk/channel4.py
+++ resources/lib/channels/uk/channel4.py
@@ -384,7 +384,7 @@ def get_video(plugin, programmeId, assetId, **kwargs):
 
     subtitle_url = ''
     if plugin.setting.get_boolean('active_subtitle'):
-        supported_subtitles_formats = ['srt_009', 'sami_001']
+        supported_subtitles_formats = ['webvtt_007', 'srt_009', 'sami_001']
         for field in json_video['subtitlesAssets']:
             if field['format'] in supported_subtitles_formats:
                 subtitle_url = field['url']
