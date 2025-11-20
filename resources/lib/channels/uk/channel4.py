ec239063b27512a4073b9d971b8a1708a851cf6a
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2025-11-20 08:51:18 +0000

    Use webvtt if Kodi >= 20

--- resources/lib/channels/uk/channel4.py
+++ resources/lib/channels/uk/channel4.py
@@ -384,7 +384,10 @@ def get_video(plugin, programmeId, assetId, **kwargs):
 
     subtitle_url = ''
     if plugin.setting.get_boolean('active_subtitle'):
-        supported_subtitles_formats = ['webvtt_007', 'srt_009', 'sami_001']
+        supported_subtitles_formats = ['srt_009', 'sami_001']
+        if get_kodi_version() >= 20:
+            supported_subtitles_formats.insert(0, 'webvtt_007')
+
         for field in json_video['subtitlesAssets']:
             if field['format'] in supported_subtitles_formats:
                 subtitle_url = field['url']
