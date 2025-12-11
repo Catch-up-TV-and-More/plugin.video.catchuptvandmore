acc19ca23c55ab1a8de8b0c6f79ec2c248d4dc18
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2025-12-11 21:47:08 +0000

    Update medi1.py

--- resources/lib/channels/ma/medi1.py
+++ resources/lib/channels/ma/medi1.py
@@ -5,6 +5,7 @@
 # This file is part of Catch-up TV & More
 
 from __future__ import unicode_literals
+import json
 import re
 import urlquick
 
@@ -13,17 +14,15 @@ from codequick import Resolver
 
 from resources.lib import resolver_proxy, web_utils
 
-URL_LIVES = 'https://idara.medi1tv.ma/rss/embed/medi1news-auto/live-medi1tv-%s.aspx'
 
 GENERIC_HEADERS = {"User-Agent": web_utils.get_random_ua()}
 
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-    if item_id == 'maghreb':
-        video_url = 'https://cdn.live.easybroadcast.io/abr_corp/83_medi1tv-maghreb_jnbspmg/playlist_dvr.m3u8'
-    else:
-        resp = urlquick.get(URL_LIVES % item_id, headers=GENERIC_HEADERS, max_age=-1)
-        video_url = re.compile(r'https?:\/\/[^\s]+\.m3u8(?:\?[^\s]*)?').findall(resp.text)[0]
-
-    return resolver_proxy.get_stream_with_quality(plugin, video_url)
+    streams = json.loads(
+        urlquick.get('https://idara.medi1tv.ma/rss/medi1tv/live.aspx', headers=GENERIC_HEADERS, max_age=-1).text)
+    for stream in streams:
+        if stream.get('titre') == item_id:
+            return resolver_proxy.get_easybroadcast_stream(plugin, stream.get('link'))
+    return None
