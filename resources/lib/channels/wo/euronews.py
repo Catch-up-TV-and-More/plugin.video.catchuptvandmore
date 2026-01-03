130f89ba79df2a338b607bc31829eaec43bac2ab
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2026-01-03 20:49:36 +0000

    [WO - EURONEWS] Fix Live

--- resources/lib/channels/wo/euronews.py
+++ resources/lib/channels/wo/euronews.py
@@ -8,35 +8,27 @@ from __future__ import unicode_literals
 import json
 
 # noinspection PyUnresolvedReferences
-from codequick import Resolver, Script
+from codequick import Listitem, Resolver, Script
 import urlquick
 
-from resources.lib import resolver_proxy
 from resources.lib import web_utils
+from resources.lib.kodi_utils import INPUTSTREAM_PROP
 
 # TODO
 # Replay add emissions
 
-URL_LIVE_API = 'https://%s.euronews.com/api/live/data?locale=%s'
-
-GENERIC_HEADERS = {'User-Agent': web_utils.get_random_ua()}
-
+URL_LIVE_API = 'https://api.euronews.com/v2/apps/androidPhoneEuronews-6.3/languages/%s/livestream'
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
     final_language = kwargs.get('language', Script.setting['euronews.language'])
     lang = final_language.lower()
-    if final_language == 'EN':
-        url_live_json = URL_LIVE_API % ('www', lang)
-    elif final_language == 'AR':
-        url_live_json = URL_LIVE_API % ('arabic', lang)
-    elif final_language == 'RO':
-        return resolver_proxy.get_stream_youtube(plugin, 'PHFADeLxJ2w', False)
-    elif final_language == 'BG':
-        return resolver_proxy.get_stream_youtube(plugin, 'P7dqy-tvU08', False)
-    else:
-        url_live_json = URL_LIVE_API % (lang, lang)
+    url_live_json = URL_LIVE_API % lang
+    json_parser = urlquick.get(url_live_json, max_age=-1).json()
+    video_url = json_parser['primary']
 
-    json_parser = urlquick.get(url_live_json, headers=GENERIC_HEADERS, max_age=-1).json()
+    item = Listitem()
+    item.path = video_url
+    item.property[INPUTSTREAM_PROP] = 'inputstream.ffmpegdirect'
 
-    return resolver_proxy.get_stream_youtube(plugin, json_parser['videoId'], False)
+    return video_url
