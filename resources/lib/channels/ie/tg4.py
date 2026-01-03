b2a815c768e77d4d2d1ef52f23871a1c21ae95e2
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2026-01-03 10:33:05 +0000

    Update tg4.py

--- resources/lib/channels/ie/tg4.py
+++ resources/lib/channels/ie/tg4.py
@@ -6,12 +6,36 @@
 
 from __future__ import unicode_literals
 
+import re
 import urlquick
 from codequick import Resolver
 
 from resources.lib import resolver_proxy
 
+URL_ROOT = 'https://www.tg4.ie'
+
+URL_LIVE = URL_ROOT + '/en/player/watch-live/home'
+
+
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-    return resolver_proxy.get_brightcove_video_json(plugin, data_account='1555966122001', data_video_id='6383454185112',
-                                                    data_player='D0RgRVjpd')
+    html_text = urlquick.get(URL_LIVE, max_age=-1).text
+
+    data_video_id = re.search(
+        r'if\s*\(\s*getCookie\("GeoCountry"\)\s*==\s*"IE"\s*\)\s*{\s*VideoID\s*=\s*(\d+);',
+        html_text,
+        re.DOTALL
+    ).group(1)
+
+    data_account = re.search(
+        r"setAttribute\(\s*['\"]data-account['\"]\s*,\s*(\d+)\s*\)",
+        html_text
+    ).group(1)
+
+    data_player = re.search(
+        r"setAttribute\(\s*['\"]data-player['\"]\s*,\s*['\"]([^'\"]+)['\"]\s*\)",
+        html_text
+    ).group(1)
+
+    return resolver_proxy.get_brightcove_video_json(plugin, data_account=data_account, data_video_id=data_video_id,
+                                                    data_player=data_player)
