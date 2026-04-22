c9ec2b827ac5940d1ee09dabd49d6364ca980a0c
Corentin Wicht <corentin.wicht@unifr.ch>
2026-04-22 11:46:04 +0200

    fixing errors in latele.py and lemanbleu.py

--- resources/lib/channels/ch/latele.py
+++ resources/lib/channels/ch/latele.py
@@ -17,7 +17,7 @@ URL_ROOT = 'https://latele.ch/'
 url_constructor = urljoin_partial(URL_ROOT)
 
 URL_LIVE = url_constructor('live')
-URL_LIVE_M3U8 = "https://latele2.vedge.infomaniak.com/livecast/latele2/manifest.m3u8"
+URL_LIVE_M3U8 = 'https://latele2.vedge.infomaniak.com/livecast/ik:latele2/manifest.m3u8'
 
 
 @Route.register
@@ -25,21 +25,6 @@ def list_programs(plugin, item_id, **kwargs):
     # TODO
     return False
 
-
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
-    # TODO get m3u8 from URL_LIVE
-
-    # headers = {
-    #     "User-Agent": get_random_ua(),
-    #     "Accept": "*/*",
-    #     "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-    #     "Sec-Fetch-Dest": "empty",
-    #     "Sec-Fetch-Mode": "cors",
-    #     "Sec-Fetch-Site": "cross-site",
-    #     "Pragma": "no-cache",
-    #     "Cache-Control": "no-cache",
-    #     "referrer": "https://latele.ch/"
-    # }
-
-    return resolver_proxy.get_stream_with_quality(plugin, video_url=URL_LIVE_M3U8, manifest_type="hls")
+    return 'https://latele2.vedge.infomaniak.com/livecast/ik:latele2/manifest.m3u8'
