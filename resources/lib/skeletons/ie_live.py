9be4fe1c6ba87708c2c4a21fc3647f04b3302b1e
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2026-06-18 15:52:06 +0000

    Add RTE

--- resources/lib/skeletons/ie_live.py
+++ resources/lib/skeletons/ie_live.py
@@ -17,14 +17,46 @@ from __future__ import unicode_literals
 root = 'live_tv'
 
 menu = {
-    'tg4': {
+    'RTEONE': {
+        'resolver': '/resources/lib/channels/ie/rte:get_live_url',
+        'label': 'RTÉ One',
+        'thumb': '',
+        'fanart': '',
+        'xmltv_id': '',
+        'enabled': True,
+        'order': 1
+    }, 'RTE2': {
+        'resolver': '/resources/lib/channels/ie/rte:get_live_url',
+        'label': 'RTÉ2',
+        'thumb': '',
+        'fanart': '',
+        'xmltv_id': '',
+        'enabled': True,
+        'order': 2
+    }, 'RTENewsNow': {
+        'resolver': '/resources/lib/channels/ie/rte:get_live_url',
+        'label': 'RTÉ News',
+        'thumb': '',
+        'fanart': '',
+        'xmltv_id': '',
+        'enabled': True,
+        'order': 3
+    }, 'RTEJr': {
+        'resolver': '/resources/lib/channels/ie/rte:get_live_url',
+        'label': 'RTÉjr',
+        'thumb': '',
+        'fanart': '',
+        'xmltv_id': '',
+        'enabled': True,
+        'order': 4
+    }, 'tg4': {
         'resolver': '/resources/lib/channels/ie/tg4:get_live_url',
         'label': 'TG4',
         'thumb': 'channels/ie/tg4.png',
         'fanart': 'channels/ie/tg4_fanart.png',
         'xmltv_id': '',
         'enabled': True,
-        'order': 1
+        'order': 5
     }, 'cula4': {
         'resolver': '/resources/lib/channels/ie/cula4:get_live_url',
         'label': 'Cúla4',
@@ -32,6 +64,6 @@ menu = {
         'fanart': 'channels/ie/cula4_fanart.png',
         'xmltv_id': '',
         'enabled': True,
-        'order': 2
+        'order': 6
     },
 }
