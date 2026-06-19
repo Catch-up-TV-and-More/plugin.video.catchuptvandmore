fed86c215118acc15151442598f15bc2545b8f9d
CasperMcFadden95 <145611964+CasperMcFadden95@users.noreply.github.com>
2026-06-19 13:26:19 +0000

    Update greatplayer.py

--- resources/lib/channels/uk/greatplayer.py
+++ resources/lib/channels/uk/greatplayer.py
@@ -134,7 +134,8 @@ def get_video(plugin, url, **kwargs):
                     video_url = video_stream.get('url')
                     license_url = drm.get('licenseUrl')
                     custom_license_headers = {h.get("name"): h.get("value") for h in drm.get("licenseTokenHeaders", [])}
-                    return resolver_proxy.get_stream_with_quality(plugin, video_url=video_url,
+                    return resolver_proxy.get_stream_with_quality(plugin,
+                                                                  video_url=video_url,
                                                                   custom_license_headers=custom_license_headers,
                                                                   license_url=license_url)
     return None
